56f50ee5a3bbb9a7e039f1c4e287841972a3fe40
Enrique Delgado <edelga34@ford.com>
2022-10-07 15:07:55 -0400

    Job Upload View WIP

--- src/routes/job/JobPhotos.jsx
+++ src/routes/job/JobPhotos.jsx
@@ -51,17 +51,59 @@ const JobPhotos = () => {
 
     return (
         <div className="mt-8">
-            <div className="grid grid-cols-2 mb-4">
-                <div className="text-gray-500 text-sm">
-                    <div className="relative top-3">Total Photos: {interiorPhotos.length + exteriorPhotos.length}</div>
+            <div className="grid grid-cols-2">
+                <div className="text-gray-500">
+                    <div className="leading-6 font-medium text-gray-600 relative top-2">Photos</div>
                 </div>
-                <div className="text-right">
-                    <button
+                <div className="text-right flex justify-end">
+                    <button type="button" class="flex items-center justify-center rounded-full bg-red-600 p-1
+                                               text-white hover:bg-red-700 focus:outline-none focus:ring-2
+                                                focus:ring-red-500 focus:ring-offset-2">
+                        <svg class="h-6 w-6" x-description="Heroicon name: outline/plus"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"></path>
+                        </svg>
+                    </button>
+                    {/* <button
                         type="button"
                         className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:w-auto"
                         >
                         Upload
-                    </button>
+                    </button> */}
+                </div>
+            </div>
+            <div>
+            <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                  <div className="space-y-1 text-center">
+                    <svg
+                      className="mx-auto h-12 w-12 text-gray-400"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                    <div className="flex text-sm text-gray-600">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer rounded-md bg-white font-medium text-red-600
+                                   focus-within:outline-none focus-within:ring-2 focus-within:ring-red-500
+                                   focus-within:ring-offset-2 hover:text-red-500"
+                      >
+                        <span>Upload a file</span>
+                        <input id="file-upload" name="file-upload" type="file" className="sr-only" multiple />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                    <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                  </div>
                 </div>
             </div>
             <div className="">
