996b63c2d9cdfb3bf24bf4667d92f74c31c31559
Enrique Delgado <edelga34@ford.com>
2022-10-07 07:11:53 -0400

    Added Image Viewer plugin and download icon

--- src/routes/job/JobPhotos.jsx
+++ src/routes/job/JobPhotos.jsx
@@ -1,18 +1,36 @@
 
 import { useEffect, useState } from "react";
-
 import { TrashIcon, CloudDownloadIcon } from "@heroicons/react/outline";
+import ImageViewer from 'react-simple-image-viewer'
 
 import * as api from './apiService'
 
+const images = [
+    'http://placeimg.com/1200/800/nature',
+    'http://placeimg.com/800/1200/nature',
+    'http://placeimg.com/1920/1080/nature',
+    'http://placeimg.com/1500/500/nature',
+  ];
 
 const JobPhotos = () => {
+    const [currentImage, setCurrentImage] = useState(0);
     const [interiorPhotos, setInteriorPhotos] = useState([])
     const [exteriorPhotos, setExteriorPhotos] = useState([])
+    const [isViewerOpen, setIsViewerOpen] = useState(false);
 
     useEffect(() => {
         getPhotos()
     }, [])
+
+    const openImageViewer = (index) => {
+        setCurrentImage(index);
+        setIsViewerOpen(true)
+    }
+
+    const closeImageViewer = () => {
+        setCurrentImage(0);
+        setIsViewerOpen(false);
+      };
 
     const getPhotos = async () => {
         try {
@@ -23,9 +41,9 @@ const JobPhotos = () => {
 
             data.results.forEach(entry => {
                 if (entry.interior) {
-                    interior_photos.push(entry)
+                    interior_photos.push(entry.image)
                 } else {
-                    exterior_photos.push(entry)
+                    exterior_photos.push(entry.image)
                 }
             });
 
@@ -59,21 +77,28 @@ const JobPhotos = () => {
                     <ul className="-my-5 divide-y divide-gray-200">
                         {interiorPhotos.length === 0 && <div className="text-gray-500 text-sm">None</div>}
 
-                        {interiorPhotos.map((photo) => (
-                            <li key={photo.id} className="py-4">
+                        {interiorPhotos.map((photo, index) => (
+                            <li key={index} className="py-4">
                                 <div className="flex items-center space-x-4">
-                                    <div className="flex-shrink-0">
-                                        <img className="h-28 w-28" src={photo.image} alt="" />
+                                    <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewer(index) }>
+                                        <img className="h-48 w-48 rounded-lg" src={photo} alt="" />
                                     </div>
-                                    <div className="min-w-0 flex-1">
-                                        <p className="truncate text-xs text-gray-500">{photo.name}</p>
-                                    </div>
-                                    <div>
+
+                                    {/* YOU HAVE TO APPEND /fl_attachment/  between upload/ and /v1 */}
+                                    {/* find the index of /upload/ and then append fl_attachment then append the rest */}
+                                    {/* https://stackoverflow.com/questions/4313841/insert-a-string-at-a-specific-index */}
+                                    <a href="https://res.cloudinary.com/datidxeqm/image/upload/fl_attachment/v1/media/images/409cf4bd-acff-4bf5-9f6b-fa8fa3ce74da_m1zxjb">
+                                        <div className="bg-red-100 text-red-700 p-2 rounded-lg hover:bg-red-200">
+                                            <CloudDownloadIcon className="flex-shrink-0 h-4 w-4" />
+                                        </div>
+                                    </a>
+                                    
+                                    <div className="flex justify-end">
                                         <div className="text-xs leading-5 font-semibold bg-slate-400/10
                                                                         p-2 text-slate-500
-                                                                        flex items-center space-x-2 hover:bg-slate-400/20
-                                                                        dark:highlight-white/5">
-                                            <TrashIcon className="flex-shrink-0 h-4 w-4 cursor-pointer"/>
+                                                                        hover:bg-slate-400/20
+                                                                        dark:highlight-white/5 rounded-lg cursor-pointer">
+                                            <TrashIcon className="flex-shrink-0 h-4 w-4"/>
                                         </div>
                                     </div>
                                 </div>
@@ -86,15 +111,18 @@ const JobPhotos = () => {
                      <ul className="-my-5 divide-y divide-gray-200">
                         {exteriorPhotos.length === 0 && <div className="text-gray-500 text-sm">None</div>}
 
-                        {exteriorPhotos.map((photo) => (
-                            <li key={photo.id} className="py-4">
+                        {exteriorPhotos.map((photo, index) => (
+                            <li key={index} className="py-4">
                                 <div className="flex items-center space-x-4">
-                                    <div className="flex-shrink-0">
-                                        <img className="h-28 w-28" src={photo.image} alt="" />
+                                    <div className="flex-shrink-0 cursor-pointer">
+                                        <img className="h-48 w-48 rounded-lg" src={photo} alt="" />
                                     </div>
-                                    <div className="min-w-0 flex-1">
-                                        <p className="truncate text-xs text-gray-500">{photo.name}</p>
-                                    </div>
+
+                                    <a href="https://res.cloudinary.com/datidxeqm/image/upload/fl_attachment/v1/media/images/409cf4bd-acff-4bf5-9f6b-fa8fa3ce74da_m1zxjb">
+                                        <div className="bg-red-100 text-red-700 p-2 rounded-lg hover:bg-red-200">
+                                            <CloudDownloadIcon className="flex-shrink-0 h-4 w-4" />
+                                        </div>
+                                    </a>
                                     <div>
                                         <div className="text-xs leading-5 font-semibold bg-slate-400/10
                                                                         p-2 text-slate-500
@@ -109,6 +137,16 @@ const JobPhotos = () => {
                     </ul>
                 </div>
             </div>
+
+            {isViewerOpen && (
+                <ImageViewer
+                src={ interiorPhotos }
+                currentIndex={ currentImage }
+                disableScroll={ false }
+                closeOnClickOutside={ true }
+                onClose={ closeImageViewer }
+                />
+            )}
             
         </div>
     )
