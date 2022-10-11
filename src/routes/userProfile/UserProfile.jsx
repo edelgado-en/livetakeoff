8797d3ee82b0be148e1ce65a5b781df9ac665a18
Enrique Delgado <edelga34@ford.com>
2022-10-10 20:45:20 -0400

    updated user profile form

--- src/routes/userProfile/UserProfile.jsx
+++ src/routes/userProfile/UserProfile.jsx
@@ -38,12 +38,6 @@ const UserProfile = () => {
         reset(user)
     }, [user])
 
-    const isValidEmail = () =>
-    // eslint-disable-next-line no-useless-escape
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        user.email
-    );
-
     const getUserDetails = async () => {
         const { data } = await api.getUsetDetails();
 
@@ -297,10 +291,10 @@ const UserProfile = () => {
                         Email
                       </label>
                       <input
-                        type="email"
+                        type="text"
                         name="url"
                         id="url"
-                        {...register('email', { required: 'Email is required', validate: isValidEmail })}
+                        {...register('email', { required: 'Email is required' })}
                         className="mt-1 block w-full rounded-md border border-gray-300
                                    py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
                       />
