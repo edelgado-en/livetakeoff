8aba2d992d92d119b871d26f6977a71ad2e54cfd
Enrique Delgado <edelga34@ford.com>
2022-10-12 15:28:43 -0400

    create job cleanup

--- src/routes/job/CreateJob.jsx
+++ src/routes/job/CreateJob.jsx
@@ -68,7 +68,7 @@ const team = [
     },
   ]
 
-const people = [
+const services = [
     "Wade Cooper",
     "Arlene Mccoy",
     "Devon Webb",
@@ -105,7 +105,7 @@ const CreateJob = () => {
         if (!isServiceSelected(value)) {
             const selectedPersonsUpdated = [
                 ...selectedServices,
-                people.find((el) => el === value)
+                services.find((el) => el === value)
             ]
             
             setSelectedServices(selectedPersonsUpdated);
@@ -151,17 +151,16 @@ const CreateJob = () => {
               </div>
             </div>
 
-            <div>
-              <div className="mt-1">
+            <div className="mt-1">
                 <Listbox value={customerSelected} onChange={setCustomerSelected}>
                     {({ open }) => (
                         <>
                         <Listbox.Label className="block text-sm font-medium text-gray-700">Customer</Listbox.Label>
                         <div className="relative mt-1">
                             <Listbox.Button className="relative w-full cursor-default rounded-md border
-                                                     border-gray-300 bg-white py-2 pl-3 pr-10 text-left
-                                                       shadow-sm focus:border-sky-500 focus:outline-none
-                                                       focus:ring-1 focus:ring-sky-500 sm:text-sm">
+                                                        border-gray-300 bg-white py-2 pl-3 pr-10 text-left
+                                                        shadow-sm focus:border-sky-500 focus:outline-none
+                                                        focus:ring-1 focus:ring-sky-500 sm:text-sm">
                                 <span className="block truncate">{customerSelected.name}</span>
                                 <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                                     <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -173,158 +172,134 @@ const CreateJob = () => {
                                 as={Fragment}
                                 leave="transition ease-in duration-100"
                                 leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                            >
-                            <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto
-                                                        rounded-md bg-white py-1 text-base shadow-lg ring-1
-                                                        ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                {customers.map((customer) => (
-                                    <Listbox.Option
-                                        key={customer.id}
-                                        className={({ active }) =>
-                                        classNames(
-                                            active ? 'text-white bg-red-600' : 'text-gray-900',
-                                            'relative cursor-default select-none py-2 pl-3 pr-9'
-                                        )
-                                        }
-                                        value={customer}
-                                    >
-                                        {({ selected, active }) => (
-                                        <>
-                                            <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                                                {customer.name}
-                                            </span>
-
-                                            {selected ? (
-                                                <span
-                                                    className={classNames(
-                                                    active ? 'text-white' : 'text-red-600',
-                                                    'absolute inset-y-0 right-0 flex items-center pr-4'
-                                                    )}
-                                                >
-                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                </span>
-                                            ) : null}
-                                        </>
-                                        )}
-                                    </Listbox.Option>
-                                ))}
-                            </Listbox.Options>
+                                leaveTo="opacity-0">
+                                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto
+                                                            rounded-md bg-white py-1 text-base shadow-lg ring-1
+                                                            ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                    {customers.map((customer) => (
+                                        <Listbox.Option
+                                            key={customer.id}
+                                            className={({ active }) =>
+                                                    classNames(active ? 'text-white bg-red-600' : 'text-gray-900',
+                                                            'relative cursor-default select-none py-2 pl-3 pr-9')}
+                                            value={customer}>
+                                            {({ selected, active }) => (
+                                                <>
+                                                    <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                                        {customer.name}
+                                                    </span>
+                                                    {selected ? (
+                                                        <span
+                                                            className={classNames(
+                                                            active ? 'text-white' : 'text-red-600',
+                                                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                            )}>
+                                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                        </span>
+                                                    ) : null}
+                                                </>
+                                            )}
+                                        </Listbox.Option>
+                                    ))}
+                                </Listbox.Options>
                             </Transition>
                         </div>
                         </>
                     )}
                 </Listbox>
-              </div>
             </div>
+
             <div>
-            <Listbox
+                <Listbox
                     as="div"
                     className="space-y-1"
                     value={selectedServices}
                     onChange={(value) => handleSelectService(value)}
-                    open={isServicesOpen}
-                    >
-                {() => (
-            <>
-              <Listbox.Label className="block text-sm leading-5 font-medium text-gray-700">
-                Services
-              </Listbox.Label>
-              <div className="relative">
-                <span className="inline-block w-full rounded-md shadow-sm">
-                  <Listbox.Button
-                    className="cursor-default relative w-full rounded-md border border-gray-300
-                             bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue
-                              focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                    onClick={() => setIsServicesOpen(!isServicesOpen)}
-                    open={isServicesOpen}
-                  >
-                    <span className="block truncate">
-                      {selectedServices.length < 1
-                        ? "Select services"
-                        : `Selected services (${selectedServices.length})`}
-                    </span>
-                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                      <svg
-                        className="h-5 w-5 text-gray-400"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        stroke="currentColor"
-                      >
-                        <path
-                          d="M7 7l3-3 3 3m0 6l-3 3-3-3"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                    </span>
-                  </Listbox.Button>
-                </span>
-
-                <Transition
-                  unmount={false}
-                  show={isServicesOpen}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                  className="absolute mt-1 w-full rounded-md bg-white shadow-lg"
-                >
-                  <Listbox.Options
-                    static
-                    className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs
-                               overflow-auto focus:outline-none sm:text-sm sm:leading-5"
-                  >
-                    {people.map((person) => {
-                      const selected = isServiceSelected(person);
-                      return (
-                        <Listbox.Option key={person} value={person}>
-                          {({ active }) => (
-                            <div
-                              className={`${
-                                active
-                                  ? "text-white bg-red-600"
-                                  : "text-gray-900"
-                              } cursor-default select-none relative py-2 pl-8 pr-4`}
-                            >
-                              <span
-                                className={`${
-                                  selected ? "font-semibold" : "font-normal"
-                                } block truncate`}
-                              >
-                                {person}
-                              </span>
-                              {selected && (
-                                <span
-                                  className={`${
-                                    active ? "text-white" : "text-red-600"
-                                  } absolute inset-y-0 left-0 flex items-center pl-1.5`}
-                                >
-                                  <svg
-                                    className="h-5 w-5"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                  >
-                                    <path
-                                      fillRule="evenodd"
-                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                      clipRule="evenodd"
-                                    />
-                                  </svg>
-                                </span>
-                              )}
-                            </div>
-                          )}
-                        </Listbox.Option>
-                      );
-                    })}
-                  </Listbox.Options>
-                </Transition>
-              </div>
-            </>
-          )}
-        </Listbox>
+                    open={isServicesOpen}>
+                    {() => (
+                    <>
+                        <Listbox.Label className="block text-sm leading-5 font-medium text-gray-700">
+                            Services
+                        </Listbox.Label>
+                        <div className="relative">
+                            <span className="inline-block w-full rounded-md shadow-sm">
+                                <Listbox.Button
+                                    onClick={() => setIsServicesOpen(!isServicesOpen)}
+                                    open={isServicesOpen}
+                                    className="cursor-default relative w-full rounded-md border border-gray-300
+                                            bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue
+                                            focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                    <span className="block truncate">
+                                        {selectedServices.length < 1
+                                                ? "Select services"
+                                                : `Selected services (${selectedServices.length})`}
+                                    </span>
+                                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                        <svg
+                                            className="h-5 w-5 text-gray-400"
+                                            viewBox="0 0 20 20"
+                                            fill="none"
+                                            stroke="currentColor">
+                                            <path
+                                                d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                                                strokeWidth="1.5"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                    </span>
+                                </Listbox.Button>
+                            </span>
+                            <Transition
+                                unmount={false}
+                                show={isServicesOpen}
+                                leave="transition ease-in duration-100"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                                className="absolute mt-1 w-full rounded-md bg-white shadow-lg">
+                                <Listbox.Options
+                                    static
+                                    className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs
+                                            overflow-auto focus:outline-none sm:text-sm sm:leading-5">
+                                    {services.map((service) => {
+                                        const selected = isServiceSelected(service);
+                                        return (
+                                            <Listbox.Option key={service} value={service}>
+                                            {({ active }) => (
+                                                <div className={`${ active ? "text-white bg-red-600": "text-gray-900"}
+                                                                 cursor-default select-none relative py-2 pl-8 pr-4`}>
+                                                    <span className={`${selected ? "font-semibold" : "font-normal"} block truncate`}>
+                                                        {service}
+                                                    </span>
+                                                    {selected && (
+                                                        <span
+                                                        className={`${
+                                                            active ? "text-white" : "text-red-600"
+                                                        } absolute inset-y-0 left-0 flex items-center pl-1.5`}>
+                                                            <svg
+                                                                className="h-5 w-5"
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 20 20"
+                                                                fill="currentColor">
+                                                                <path
+                                                                    fillRule="evenodd"
+                                                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                                    clipRule="evenodd"
+                                                                />
+                                                            </svg>
+                                                        </span>
+                                                    )}
+                                                </div>
+                                            )}
+                                            </Listbox.Option>
+                                        );
+                                    })}
+                                </Listbox.Options>
+                            </Transition>
+                        </div>
+                    </>
+                    )}
+                </Listbox>
             </div>
 
             <div className="space-y-2">
