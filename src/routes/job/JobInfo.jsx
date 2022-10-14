943867be208b0c11d0c5db297f7b7ac10a391c22
Enrique Delgado <edelga34@ford.com>
2022-10-14 13:00:36 -0400

    Added job details title

--- src/routes/job/JobInfo.jsx
+++ src/routes/job/JobInfo.jsx
@@ -137,7 +137,12 @@ const JobInfo = () => {
 
             {!loading && errorMessage == null && (
                 <div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
-                <div className="mt-8 mb-8">
+                <div className="flex flex-row">
+                    <div className="flex-1">
+                        <div className="leading-6 font-medium text-gray-600">Job Details</div>
+                    </div>
+                </div>
+                <div className="mt-4 mb-8">
                     {jobDetails.status === 'S' && 
                         <button
                             type="button"
