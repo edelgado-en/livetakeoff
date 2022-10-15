b757fd5a1951b455f72ef3917ea0d5c0508ab63a
Enrique Delgado <edelga34@ford.com>
2022-10-15 14:04:07 -0400

    Added support for customer uploaded photo

--- src/routes/job/JobPhotoListing.jsx
+++ src/routes/job/JobPhotoListing.jsx
@@ -15,14 +15,17 @@ import photosIcon from './photosIcon.png'
 const JobPhotoListing = () => {
     const [currentImageInterior, setCurrentImageInterior] = useState(0);
     const [currentImageExterior, setCurrentImageExterior] = useState(0);
+    const [currentImageCustomer, setCurrentImageCustomer] = useState(0);
    
     const [isViewerOpenInterior, setIsViewerOpenInterior] = useState(false);
     const [isViewerOpenExterior, setIsViewerOpenExterior] = useState(false);
+    const [isViewerOpenCustomer, setIsViewerOpenCustomer] = useState(false)
 
     const [loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null)
     const [interiorPhotos, setInteriorPhotos] = useState([])
     const [exteriorPhotos, setExteriorPhotos] = useState([])
+    const [customerPhotos, setCustomerPhotos] = useState([])
     
    
     const { jobId } = useParams();
@@ -41,6 +44,11 @@ const JobPhotoListing = () => {
         setIsViewerOpenExterior(true)
     }
 
+    const openImageViewerCustomer = (index) => {
+        setCurrentImageCustomer(index);
+        setIsViewerOpenCustomer(true)
+    }
+
     const closeImageViewer = () => {
         setCurrentImageInterior(0);
         setIsViewerOpenInterior(false);
@@ -51,6 +59,11 @@ const JobPhotoListing = () => {
         setIsViewerOpenExterior(false);
     };
 
+    const closeImageViewerCustomer = () => {
+        setCurrentImageCustomer(0);
+        setIsViewerOpenCustomer(false);
+    };
+
     const handleDeletePhoto = async(photoId, isInterior) => {
         const request = {
             photos: [photoId]
@@ -58,7 +71,7 @@ const JobPhotoListing = () => {
 
         try {
             await api.deletePhotos(jobId, request)
-
+            
             if (isInterior) {
                 const updatedPhotos = interiorPhotos.filter(p => {
                     return p.id !== photoId 
@@ -79,9 +92,30 @@ const JobPhotoListing = () => {
         } catch (error) {
 
         }
-
     }
 
+    const handleDeleteCustomerPhoto = async (photoId) => {
+        
+        const request = {
+            photos: [photoId]
+        }
+
+        try {
+            await api.deletePhotos(jobId, request)
+                
+            const updatedPhotos = customerPhotos.filter(p => {
+                    return p.id !== photoId 
+                  })
+
+            setCustomerPhotos(updatedPhotos)
+            
+
+            toast.error('Photo deleted!')
+
+        } catch (error) {
+
+        }
+    }
 
     const getPhotos = async () => {
         setLoading(true)
@@ -91,16 +125,23 @@ const JobPhotoListing = () => {
 
             const interior_photos = []
             const exterior_photos = []
+            const customer_photos = []
 
             data.results.forEach(entry => {
-                if (entry.interior) {
-                    interior_photos.push(entry)
+                console.log(entry)
+                if (entry.customer_uploaded) {
+                    customer_photos.push(entry)
+
                 } else {
-                    exterior_photos.push(entry)
+                    if (entry.interior) {
+                        interior_photos.push(entry)
+                    } else {
+                        exterior_photos.push(entry)
+                    }
                 }
             });
 
-
+            setCustomerPhotos(customer_photos)
             setInteriorPhotos(interior_photos)
             setExteriorPhotos(exterior_photos)
 
@@ -125,6 +166,42 @@ const JobPhotoListing = () => {
 
             {!loading && errorMessage == null && (
                 <div className="">
+                {customerPhotos.length > 0 && (
+                    <>
+                    <div className="text-gray-500 text-sm mb-1 font-semibold mt-8">
+                        Customer Provided
+                        <span className="bg-gray-100 text-gray-700 hidden ml-2 py-0.5 px-2.5
+                                          rounded-full text-xs font-medium md:inline-block">{customerPhotos.length}</span>
+                    </div>
+                    <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-1 pl-9 xl:pl-1">
+                        {customerPhotos.map((photo, index) => (
+                            <div key={index} className="py-4">
+                                    <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewerCustomer(index) }>
+                                        <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
+                                    </div>
+                                    <div className="flex text-gray-500 text-sm pt-2">
+                                        <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
+                                        <TrashIcon 
+                                            onClick={() => handleDeleteCustomerPhoto(photo.id, true)}
+                                            className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer"/>
+                                        <span className="text-gray-500 text-sm">
+                                            <ReactTimeAgo date={new Date(photo.created_at)} locale="en-US" timeStyle="twitter" />
+                                        </span>
+                                        <span className="text-sm ml-3">{photo.name}</span>
+                                    </div>
+                                    {/* YOU HAVE TO APPEND /fl_attachment/  between upload/ and /v1 */}
+                                    {/* find the index of /upload/ and then append fl_attachment then append the rest */}
+                                    {/* https://stackoverflow.com/questions/4313841/insert-a-string-at-a-specific-index */}
+                                    {/* <a href="https://res.cloudinary.com/datidxeqm/image/upload/fl_attachment/v1/media/images/409cf4bd-acff-4bf5-9f6b-fa8fa3ce74da_m1zxjb">
+                                        */}
+                            </div>
+                        ))}
+                    </div>
+                    
+                    </>
+                )}
+
+
                 <div>
                     <div className="text-gray-500 text-sm mb-1 font-semibold mt-8">
                         Interior
@@ -154,9 +231,6 @@ const JobPhotoListing = () => {
                                     <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewer(index) }>
                                         <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
                                     </div>
-                                    {/* <div className="pt-2 flex text-gray-500 text-sm">
-                                        {photo.name}
-                                    </div> */}
                                     <div className="flex text-gray-500 text-sm pt-2">
                                         <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
                                         <TrashIcon 
@@ -223,7 +297,7 @@ const JobPhotoListing = () => {
                         ))}
                     </div>
                 </div>
-            </div>
+                </div>
             )}
             
 
@@ -244,6 +318,16 @@ const JobPhotoListing = () => {
                 disableScroll={ false }
                 closeOnClickOutside={ true }
                 onClose={ closeImageViewerExterior }
+                />
+            )}
+
+            {isViewerOpenCustomer && (
+                <ImageViewer
+                    src={ customerPhotos.map(p => p.image) }
+                    currentIndex={ currentImageCustomer }
+                    disableScroll={ false }
+                    closeOnClickOutside={ true }
+                    onClose={ closeImageViewerCustomer }
                 />
             )}
             
