9d22ff165c875773adad04cbef1a0a5d9fd7b875
Enrique Delgado <edelga34@ford.com>
2022-10-20 06:08:24 -0400

    Added customer provided photos to download zip

--- src/routes/job/JobPhotoListing.jsx
+++ src/routes/job/JobPhotoListing.jsx
@@ -67,15 +67,17 @@ const JobPhotoListing = () => {
         setIsViewerOpenCustomer(false);
     };
 
-    const downloadAllInterior = async () => {
+    const downloadAllPhotos = async () => {
         //get job because you need PO, tailNumber, airport initials
 
-        const interiorImages = interiorPhotos.map(p => {return {'interior': true, 'image': p.image}});
-        const exteriorImages = exteriorPhotos.map(p => {return {'interior': false, 'image': p.image}});
+        const interiorImages = interiorPhotos.map(p => {return {'customer': false, 'interior': true, 'image': p.image}});
+        const exteriorImages = exteriorPhotos.map(p => {return {'customer': false, 'interior': false, 'image': p.image}});
+        const customerImages = customerPhotos.map(p => {return {'customer': true, 'image': p.image}});
 
-        const allImages = [...interiorImages, ...exteriorImages]
+        const allImages = [...customerImages, ...interiorImages, ...exteriorImages]
 
         var zip = new JSZip();
+        let customerProvided = zip.folder('customer_provided')
         let interiorFolder = zip.folder('interior_photos');
         let exteriorFolder = zip.folder('exterior_photos');
 
@@ -90,14 +92,19 @@ const JobPhotoListing = () => {
           const response = await fetch(image.image);
           
           response.blob().then(blob => {
-                if (image.interior) {
-                    interiorFolder.file(filename, blob, { binary: true });
-
+                if (image.customer) {
+                    customerProvided.file(filename, blob, { binary: true });
                 } else {
-                    exteriorFolder.file(filename, blob, { binary: true });
+                    if (image.interior) {
+                        interiorFolder.file(filename, blob, { binary: true });
+    
+                    } else {
+                        exteriorFolder.file(filename, blob, { binary: true });
+                    }
                 }
+
                 count++;
-                if (count === interiorImages.length) {
+                if (count === allImages.length) {
                     zip.generateAsync({ type: 'blob' }).then(function (content) {
                       saveAs(content, zipFilename);
                     });
@@ -205,94 +212,92 @@ const JobPhotoListing = () => {
                 <div className="">
                 {customerPhotos.length > 0 && (
                     <>
-                    <div className="text-gray-500 text-lg mb-1 font-semibold mt-8">
-                        Customer Provided
-                        <span className="bg-gray-100 text-gray-700 ml-2 py-0.5 px-2.5
+                    <div className="text-gray-500 text-lg mt-4 flex justify-between">
+                        <div>
+                            <span className="font-semibold">Customer Provided</span>
+                            <span className="bg-gray-100 text-gray-700 ml-2 py-0.5 px-2.5
                                           rounded-full text-xs font-medium md:inline-block">{customerPhotos.length}</span>
+                        </div>
+                        <div className="flex justify-end">
+                            <button 
+                                className="rounded-lg p-2 cursor-pointer border hover:bg-gray-50 text-sm"
+                                onClick={() => downloadAllPhotos()}>
+                                Download all
+                            </button>
+                        </div>
                     </div>
                     <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-1 pl-9 xl:pl-1">
                         {customerPhotos.map((photo, index) => (
                             <div key={index} className="py-4">
-                                    <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewerCustomer(index) }>
-                                        <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
-                                    </div>
-                                    <div className="flex text-gray-500 text-sm pt-2">
-                                        <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
-                                        <TrashIcon 
-                                            onClick={() => handleDeleteCustomerPhoto(photo.id, true)}
-                                            className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer"/>
-                                        <span className="text-gray-500 text-sm">
-                                            <ReactTimeAgo date={new Date(photo.created_at)} locale="en-US" timeStyle="twitter" />
-                                        </span>
-                                        <span className="text-sm ml-3">{photo.name}</span>
-                                    </div>
-                                    {/* YOU HAVE TO APPEND /fl_attachment/  between upload/ and /v1 */}
-                                    {/* find the index of /upload/ and then append fl_attachment then append the rest */}
-                                    {/* https://stackoverflow.com/questions/4313841/insert-a-string-at-a-specific-index */}
-                                    {/* <a href="https://res.cloudinary.com/datidxeqm/image/upload/fl_attachment/v1/media/images/409cf4bd-acff-4bf5-9f6b-fa8fa3ce74da_m1zxjb">
-                                        */}
+                                <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewerCustomer(index) }>
+                                    <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
+                                </div>
+                                <div className="flex text-gray-500 text-sm pt-2">
+                                    <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
+                                    <TrashIcon 
+                                        onClick={() => handleDeleteCustomerPhoto(photo.id, true)}
+                                        className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer"/>
+                                    <span className="text-gray-500 text-sm">
+                                        <ReactTimeAgo date={new Date(photo.created_at)} locale="en-US" timeStyle="twitter" />
+                                    </span>
+                                    <span className="text-sm ml-3">{photo.name}</span>
+                                </div>
                             </div>
                         ))}
                     </div>
-                    
                     </>
                 )}
 
-
                 <div>
-                    <div className="text-gray-500 text-lg mb-1 font-semibold mt-8">
-                        Interior 
-                        {interiorPhotos.length > 0 &&
-                            <span className="bg-gray-100 text-gray-700 ml-2 py-0.5 px-2.5
-                                          rounded-full text-xs font-medium md:inline-block">{interiorPhotos.length}</span>
-                        }
-                    </div>
-                    {interiorPhotos.length > 0 && (
-                        <div className="flex justify-end">
-                            <button 
-                                className="rounded-lg p-2 cursor-pointer border mr-3 hover:bg-gray-50 text-sm"
-                                onClick={() => downloadAllInterior()}>
-                                Download all
-                            </button>
+                    <div className="text-gray-500 text-lg mt-4 flex justify-between">
+                        <div className="relative top-2">
+                            <span className="font-semibold">Interior</span> 
+                            {interiorPhotos.length > 0 &&
+                                <span className="bg-gray-100 text-gray-700 ml-2 py-0.5 px-2.5
+                                            rounded-full text-xs font-medium md:inline-block">{interiorPhotos.length}</span>
+                            }
                         </div>
-                    )}
-                    
+                        {(interiorPhotos.length > 0 || exteriorPhotos.length > 0) && customerPhotos.length === 0 && (
+                            <div className="flex justify-end">
+                                <button 
+                                    className="rounded-lg p-2 cursor-pointer border hover:bg-gray-50 text-sm"
+                                    onClick={() => downloadAllPhotos()}>
+                                    Download all
+                                </button>
+                            </div>
+                        )}
+                    </div>
 
                     {interiorPhotos.length === 0 && (
-                            <div className="text-sm mt-10 flex flex-col items-center">
-                                 <img
-                                    className="block h-8 w-auto text-gray-200"
-                                    src={photosIcon}
-                                    alt="photos"
-                                /> 
-                                <div className="font-semibold text-gray-600 mt-4">
-                                    No photos uploaded
-                                </div>
-                                <span className="text-gray-500">Click on the plus icon to upload some photos.</span>                            
+                        <div className="text-sm mt-10 flex flex-col items-center">
+                                <img
+                                className="block h-8 w-auto text-gray-200"
+                                src={photosIcon}
+                                alt="photos"
+                            /> 
+                            <div className="font-semibold text-gray-600 mt-4">
+                                No photos uploaded
                             </div>
+                            <span className="text-gray-500">Click on the plus icon to upload some photos.</span>                            
+                        </div>
                     )}
                     
                     <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-1 pl-9 xl:pl-1">
                         {interiorPhotos.map((photo, index) => (
                             <div key={index} className="py-4">
-                                    <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewer(index) }>
-                                        <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
-                                    </div>
-                                    <div className="flex text-gray-500 text-sm pt-2">
-                                        <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
-                                        <TrashIcon 
-                                            onClick={() => handleDeletePhoto(photo.id, true)}
-                                            className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer"/>
-                                        <span className="text-gray-500 text-sm">
-                                            <ReactTimeAgo date={new Date(photo.created_at)} locale="en-US" timeStyle="twitter" />
-                                        </span>
-                                        <span className="text-sm ml-3">{photo.name}</span>
-                                    </div>
-                                    {/* YOU HAVE TO APPEND /fl_attachment/  between upload/ and /v1 */}
-                                    {/* find the index of /upload/ and then append fl_attachment then append the rest */}
-                                    {/* https://stackoverflow.com/questions/4313841/insert-a-string-at-a-specific-index */}
-                                    {/* <a href="https://res.cloudinary.com/datidxeqm/image/upload/fl_attachment/v1/media/images/409cf4bd-acff-4bf5-9f6b-fa8fa3ce74da_m1zxjb">
-                                        */}
+                                <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewer(index) }>
+                                    <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
+                                </div>
+                                <div className="flex text-gray-500 text-sm pt-2">
+                                    <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
+                                    <TrashIcon 
+                                        onClick={() => handleDeletePhoto(photo.id, true)}
+                                        className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer"/>
+                                    <span className="text-gray-500 text-sm">
+                                        <ReactTimeAgo date={new Date(photo.created_at)} locale="en-US" timeStyle="twitter" />
+                                    </span>
+                                    <span className="text-sm ml-3">{photo.name}</span>
+                                </div>
                             </div>
                         ))}
                     </div>
@@ -321,25 +326,19 @@ const JobPhotoListing = () => {
                     <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-1 pl-9 xl:pl-1">
                         {exteriorPhotos.map((photo, index) => (
                             <div key={index} className="py-4">
-                                    <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewerExterior(index) }>
-                                        <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
-                                    </div>
-                                    <div className="flex text-gray-500 text-sm pt-2">
-                                        <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
-                                        <TrashIcon 
-                                            onClick={() => handleDeletePhoto(photo.id, false)}
-                                            className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer"/>
-                                        <span className="text-gray-500 text-sm">
-                                            <ReactTimeAgo date={new Date(photo.created_at)} locale="en-US" timeStyle="twitter" />
-                                        </span>
-                                        <span className="text-sm ml-3 truncate w-44 text-ellipsis overflow-hidden whitespace-nowrap">{photo.name}</span>
-                                    </div>
-
-                                    {/* YOU HAVE TO APPEND /fl_attachment/  between upload/ and /v1 */}
-                                    {/* find the index of /upload/ and then append fl_attachment then append the rest */}
-                                    {/* https://stackoverflow.com/questions/4313841/insert-a-string-at-a-specific-index */}
-                                    {/* <a href="https://res.cloudinary.com/datidxeqm/image/upload/fl_attachment/v1/media/images/409cf4bd-acff-4bf5-9f6b-fa8fa3ce74da_m1zxjb">
-                                        */}
+                                <div className="flex-shrink-0 cursor-pointer" onClick={ () => openImageViewerExterior(index) }>
+                                    <img className="h-60 w-72 rounded-lg" src={photo.image} alt="" />
+                                </div>
+                                <div className="flex text-gray-500 text-sm pt-2">
+                                    <CloudDownloadIcon className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer" />
+                                    <TrashIcon 
+                                        onClick={() => handleDeletePhoto(photo.id, false)}
+                                        className="flex-shrink-0 h-4 w-4 mr-2 cursor-pointer"/>
+                                    <span className="text-gray-500 text-sm">
+                                        <ReactTimeAgo date={new Date(photo.created_at)} locale="en-US" timeStyle="twitter" />
+                                    </span>
+                                    <span className="text-sm ml-3 truncate w-44 text-ellipsis overflow-hidden whitespace-nowrap">{photo.name}</span>
+                                </div>
                             </div>
                         ))}
                     </div>
