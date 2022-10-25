e974079ebdc5b5dfaa34d80f5f8c71d317fa7c1b
Enrique Delgado <edelga34@ford.com>
2022-10-25 06:41:38 -0400

    Added modal for delete discount. Added general description

--- src/routes/customers/CustomerDiscountList.jsx
+++ src/routes/customers/CustomerDiscountList.jsx
@@ -3,12 +3,15 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { TrashIcon, PencilIcon } from "@heroicons/react/outline";
 import AnimatedPage from "../../components/animatedPage/AnimatedPage";
+import DeleteDiscountModal from "./DeleteDiscountModal";
 import * as api from './apiService'
 
 const CustomerDiscountList = () => {
     const { customerId } = useParams();
     const [discounts, setDiscounts] = useState([])
     const [loading, setLoading] = useState(false)
+    const [isDeleteDiscountModalOpen, setDeleteDiscountModalOpen] = useState(false)
+    const [discountToBeDeleted, setDiscountToBeDeleted] = useState(null)
 
     useEffect(() => {
         getDiscounts()
@@ -23,10 +26,22 @@ const CustomerDiscountList = () => {
         setDiscounts(data)
     }
 
-    const handleDeleteDiscount = async (discountId) => {
-        await api.deleteDiscount(discountId)
+    const deleteDiscount = async (discount) => {
+        await api.deleteDiscount(discount.id)
+
+        setDeleteDiscountModalOpen(false)
 
         getDiscounts()
+    }
+
+    const handleToggleDeleteDiscountModal = (discount) => {
+        if (discount) {
+            setDiscountToBeDeleted(discount)
+        } else {
+            setDiscountToBeDeleted(null)
+        }
+
+        setDeleteDiscountModalOpen(!isDeleteDiscountModalOpen)
     }
 
     return (
@@ -40,7 +55,7 @@ const CustomerDiscountList = () => {
                 </div>
             )}
 
-            <div className="overflow-hidden bg-white shadow sm:rounded-md">
+            <div className="overflow-hidden bg-white shadow sm:rounded-md mb-20">
                 <ul role="list" className="divide-y divide-gray-200">
                     {discounts.map((discount) => (
                     <li key={discount.id}>
@@ -61,25 +76,36 @@ const CustomerDiscountList = () => {
                             </div>
                             </div>
                             <div className="mt-5 sm:flex sm:justify-between">
-                            <div className="text-sm text-gray-500">
-                                {discount.airports.map((airport, index) => (
-                                    <div key={index} className="mb-2">{index + 1 + '. '}{airport.name}</div>
-                                ))}
+                                {discount.type === 'G' && (
+                                    <p className="text-gray-500 text-sm">Applies to every job for this customer</p>
+                                )}
+                                <div className="text-sm text-gray-500">
+                                    {discount.airports.map((airport, index) => (
+                                        <div key={index} className="mb-2">{index + 1 + '. '}{airport.name}</div>
+                                    ))}
 
-                                {discount.services.map((service, index) => (
-                                    <div key={index} className="mb-2">{index + 1 + '. '}{service.name}</div>
-                                ))}
-                            </div>
-                            <div className="mt-5 flex items-center text-sm text-gray-500 sm:mt-0">
-                                <PencilIcon className="flex-shrink-0 h-4 w-4 mr-6 cursor-pointer" />
-                                <TrashIcon onClick={() => handleDeleteDiscount(discount.id)} className="flex-shrink-0 h-4 w-4 cursor-pointer"/>
-                            </div>
+                                    {discount.services.map((service, index) => (
+                                        <div key={index} className="mb-2">{index + 1 + '. '}{service.name}</div>
+                                    ))}
+                                </div>
+                                <div className="mt-5 flex items-center text-sm text-gray-500 sm:mt-0">
+                                    <PencilIcon className="flex-shrink-0 h-4 w-4 mr-6 cursor-pointer" />
+                                    <TrashIcon onClick={() => handleToggleDeleteDiscountModal(discount)}
+                                               className="flex-shrink-0 h-4 w-4 cursor-pointer"/>
+                                </div>
                             </div>
                         </div>
                         </div>
                     </li>
                     ))}
                 </ul>
+
+                {isDeleteDiscountModalOpen && <DeleteDiscountModal 
+                                                isOpen={isDeleteDiscountModalOpen}
+                                                handleClose={handleToggleDeleteDiscountModal}
+                                                deleteDiscount={deleteDiscount}
+                                                discount={discountToBeDeleted}
+                                            />}
             </div>
         </AnimatedPage>
     )
