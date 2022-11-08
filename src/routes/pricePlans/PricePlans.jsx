14abe54e61b07e382fc316ae0d9c1c0a28548a67
Enrique Delgado <edelga34@ford.com>
2022-11-08 08:54:53 -0500

    Added ability to delete price list

--- src/routes/pricePlans/PricePlans.jsx
+++ src/routes/pricePlans/PricePlans.jsx
@@ -3,51 +3,16 @@ import AnimatedPage from "../../components/animatedPage/AnimatedPage";
 import Loader from "../../components/loader/Loader";
 import { ChevronLeftIcon, CheckIcon, PlusIcon, TrashIcon, PencilIcon, UserIcon, CalendarIcon, UsersIcon } from '@heroicons/react/outline'
 import { Link } from 'react-router-dom';
+import PricePlanDeleteModal from "./PricePlanDeleteModal";
 
 import * as api from './apiService'
 
-const tiers = [
-    {
-      id: 1,
-      name: 'Standard',
-      href: '#',
-      description: 'All the basics for starting a new client',
-      createdBy: 'System',
-      createdAt: '11/06/2022',
-      customersUsingIt: 55
-    },
-    {
-      id: 2,
-      name: 'Global Appeareance',
-      href: '#',
-      description: '+15% for all services. Custom based.',
-      createdBy: 'edelgado',
-      createdAt: '11/06/2022',
-      customersUsingIt: 1
-    },
-    {
-      id: 3,
-      name: 'Holiday Special',
-      href: '#',
-      description: '-10% for all services. Used this list in December',
-      createdBy: 'System',
-      createdAt: '11/06/2022',
-      customersUsingIt: 10
-    },
-    {
-      id: 4,
-      name: 'Enterprise',
-      href: '#',
-      description: '+10% for all services for big clients',
-      createdBy: 'System',
-      createdAt: '11/06/2022',
-      customersUsingIt: 1
-    },
-  ]
 
 const PricePlans = () => {
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false)
     const [pricingPlans, setPricingPlans] = useState([]);
+    const [isDeletePricePlanModalOpen, setDeletePricePlanModalOpen] = useState(false)
+    const [pricePlanToBeDeleted, setPricePlanToBeDeleted] = useState(null)
 
     useEffect(() => {
         getPricingPlans()
@@ -56,6 +21,26 @@ const PricePlans = () => {
     const getPricingPlans = async () => {
         const { data } = await api.getPricingPlans()
         setPricingPlans(data.results)
+    }
+
+    const handleToggleDeletePricePlanModal = (pricePlan) => {
+        setPricePlanToBeDeleted(pricePlan)
+        setDeletePricePlanModalOpen(!isDeletePricePlanModalOpen)
+    }
+
+    const handleDeletePricePlan = async (pricePlan) => {
+        setDeletePricePlanModalOpen(false)
+        setLoading(true)
+        
+        try {
+            await api.deletePricePlan(pricePlan.id)
+            getPricingPlans()
+
+            setLoading(false)
+
+        } catch (error) {
+            setLoading(false)
+        }
     }
 
     return (
@@ -81,50 +66,64 @@ const PricePlans = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="mt-10 grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols1 sm:grid-cols-1 xs:grid-cols-1 gap-6">
-                    {pricingPlans.map((pricingPlan) => (
-                        <div key={pricingPlan.name} className="divide-y divide-gray-200 rounded-lg border border-gray-200 shadow-sm">
-                            <div className="p-6">
-                                <div className="flex justify-between">
-                                    <h2 className="text-lg font-medium leading-6 text-gray-900">{pricingPlan.name}</h2>
-                                    {pricingPlan.name !== 'Standard' && (
-                                        <div className="flex gap-3">
-                                            <PencilIcon className="h-4 w-4 text-gray-500 cursor-pointer" />
-                                            <TrashIcon className="h-4 w-4 text-gray-500 cursor-pointer" />
-                                        </div>
-                                    )}
-                                </div>
-                                <p className="mt-4 text-sm text-gray-500" style={{ minHeight: '60px' }}>{pricingPlan.description}</p>
-                                <Link
-                                    to={`${pricingPlan.id}`}
-                                    className="mt-8 block w-full rounded-md border border-transparent
-                                            bg-red-600 py-2 text-center text-sm font-semibold
-                                            text-white hover:bg-red-700"
-                                >
-                                    See prices
-                                </Link>
-                            </div>
-                            <div className="px-6 pt-2 pb-8">
-                                <ul role="list" className="mt-6 space-y-4">
-                                    <li className="flex space-x-3">
-                                        <UserIcon className="h-4 w-4 flex-shrink-0 text-gray-500" aria-hidden="true" />
-                                        <span className="text-xs text-gray-500">Created by {pricingPlan.created_by ? pricingPlan.created_by.username : 'System'}</span>
-                                    </li>
-                                    <li className="flex space-x-3">
-                                        <CalendarIcon className="h-4 w-4 flex-shrink-0 text-gray-500" aria-hidden="true" />
-                                        <span className="text-xs text-gray-500">Created on {pricingPlan.created_at}</span>
-                                    </li>
-                                    <li className="flex space-x-3">
-                                        <UsersIcon className="h-4 w-4 flex-shrink-0 text-gray-500" aria-hidden="true" />
-                                        <span className="text-xs text-gray-500">{pricingPlan.num_customers} customer(s) currently using it</span>
-                                    </li>
 
-                                </ul>
+                {loading && <Loader />}
+                
+                {!loading && (
+                    <div className="mt-10 grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols1 sm:grid-cols-1 xs:grid-cols-1 gap-6">
+                        {pricingPlans.map((pricingPlan) => (
+                            <div key={pricingPlan.name} className="divide-y divide-gray-200 rounded-lg border border-gray-200 shadow-sm">
+                                <div className="p-6">
+                                    <div className="flex justify-between">
+                                        <h2 className="text-lg font-medium leading-6 text-gray-900">{pricingPlan.name}</h2>
+                                        {pricingPlan.name !== 'Standard' && (
+                                            <div className="flex gap-3">
+                                                <PencilIcon className="h-4 w-4 text-gray-500 cursor-pointer" />
+                                                <TrashIcon 
+                                                    onClick={() => handleToggleDeletePricePlanModal(pricingPlan)}
+                                                    className="h-4 w-4 text-gray-500 cursor-pointer" />
+                                            </div>
+                                        )}
+                                    </div>
+                                    <p className="mt-4 text-sm text-gray-500" style={{ minHeight: '60px' }}>{pricingPlan.description}</p>
+                                    <Link
+                                        to={`${pricingPlan.id}`}
+                                        className="mt-8 block w-full rounded-md border border-transparent
+                                                bg-red-600 py-2 text-center text-sm font-semibold
+                                                text-white hover:bg-red-700"
+                                    >
+                                        See prices
+                                    </Link>
+                                </div>
+                                <div className="px-6 pt-2 pb-8">
+                                    <ul role="list" className="mt-6 space-y-4">
+                                        <li className="flex space-x-3">
+                                            <UserIcon className="h-4 w-4 flex-shrink-0 text-gray-500" aria-hidden="true" />
+                                            <span className="text-xs text-gray-500">Created by {pricingPlan.created_by ? pricingPlan.created_by.username : 'System'}</span>
+                                        </li>
+                                        <li className="flex space-x-3">
+                                            <CalendarIcon className="h-4 w-4 flex-shrink-0 text-gray-500" aria-hidden="true" />
+                                            <span className="text-xs text-gray-500">Created on {pricingPlan.created_at}</span>
+                                        </li>
+                                        <li className="flex space-x-3">
+                                            <UsersIcon className="h-4 w-4 flex-shrink-0 text-gray-500" aria-hidden="true" />
+                                            <span className="text-xs text-gray-500">{pricingPlan.num_customers} customer(s) currently using it</span>
+                                        </li>
+
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
+                
             </article>
+
+            {isDeletePricePlanModalOpen && <PricePlanDeleteModal
+                                            isOpen={isDeletePricePlanModalOpen}
+                                            priceList={pricePlanToBeDeleted}
+                                            handleClose={handleToggleDeletePricePlanModal}
+                                            deletePriceList={handleDeletePricePlan} />}
         </AnimatedPage>
     )
 }
--- src/routes/pricePlans/apiService.ts
+++ src/routes/pricePlans/apiService.ts
@@ -13,6 +13,10 @@ export const createPricingPlan = (request: any) => {
     return httpService.post('/api/pricing-plans', request)
 }
 
+export const deletePricePlan = (id: number) => {
+    return httpService.delete(`/api/pricing-plans/${id}/`)
+}
+
 export const getPriceListing = (id: number) => {
     return httpService.get(`/api/price-listing/${id}/`);
 }
