19b504ab3e9f68efbf9bea48550676c628f9a1d4
Enrique Delgado <edelga34@ford.com>
2022-11-17 12:37:04 -0500

    Fixed misalignment in job details

--- src/routes/job/JobDetails.jsx
+++ src/routes/job/JobDetails.jsx
@@ -92,7 +92,7 @@ const JobDetails = () => {
                                 </Link>
                                 <Link
                                     to="comments">
-                                    <div className="flex">
+                                    <div className="">
                                         <svg xmlns="http://www.w3.org/2000/svg"
                                              fill="none"
                                               viewBox="0 0 24 24"
@@ -100,7 +100,7 @@ const JobDetails = () => {
                                           <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
                                         </svg>
                                         {jobStats.comments_count > 0 && (
-                                            <div className="bg-red-500 text-white py-1 px-2 relative bottom-2 right-2
+                                            <div className="bg-red-500 text-white py-1 px-2 relative bottom-9 left-3
                                                             rounded-full text-xs font-medium inline-block scale-90">
                                                 {jobStats.comments_count}
                                             </div>
@@ -109,11 +109,11 @@ const JobDetails = () => {
                                 </Link>
                                 <Link
                                     to="photos/listing">
-                                    <div className="flex">
+                                    <div className="">
                                         <PhotographIcon className="h-6 w-6 text-gray-600"/>
                                         {jobStats.photos_count > 0 && (
                                             <div className="bg-gray-50 text-gray-600 py-1
-                                                             relative bottom-2 border right-2 px-2
+                                                             relative bottom-9 border left-3 px-2
                                                              rounded-full text-xs font-medium inline-block scale-90">
                                                 {jobStats.photos_count}
                                             </div>
