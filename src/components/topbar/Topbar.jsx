c1aa8bf9eee9183fb474dce0383399b89240c36b
Enrique Delgado <edelga34@ford.com>
2022-11-20 15:13:03 -0500

    Added improvements to the profile popover to account information

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -169,14 +169,28 @@ const Topbar = () => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-60
+                                           origin-top-right rounded-md bg-white py-1 shadow-lg ring-1
+                                            ring-black ring-opacity-5 focus:outline-none">
+                      <p className="truncate py-3 px-3.5 border-b">
+                          <div className="block text-xs text-gray-500">Signed in as</div>
+                          <div className="mt-0.5 text-sm text-gray-700 py-1">
+                            {currentUser.isAdmin ? 'Admin'
+                              : currentUser.isSuperUser ? 'Super User'
+                              : currentUser.isAccountManager ? 'Account Manager'
+                              : currentUser.isCustomer ? 'Customer'
+                              : 'Project Manager'}
+                          </div>
+                          <div className="text-sm font-semibold text-gray-700">{currentUser.email}</div>
+                      </p>
+                      
                       <Menu.Item>
                         {({ active }) => (
                           <Link
-                            to="user-settings/profile"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            to="contact"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-3 text-sm text-gray-700')}
                           >
-                            Your Profile
+                            Contact Us
                           </Link>
                         )}
                       </Menu.Item>
@@ -184,13 +198,14 @@ const Topbar = () => {
                       <Menu.Item>
                         {({ active }) => (
                           <Link
-                            to="contact"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            to="user-settings/profile"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 border-t border-gray-200 pt-4')}
                           >
-                            Contact Us
+                            Account Settings
                           </Link>
                         )}
                       </Menu.Item>
+
                       
                       <Menu.Item>
                         {({ active }) => (
