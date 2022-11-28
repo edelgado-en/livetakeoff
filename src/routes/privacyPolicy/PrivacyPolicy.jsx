fbbb78bf260b1f0734549bd3260409cf209990bc
Enrique Delgado <edelga34@ford.com>
2022-11-28 12:35:22 -0500

    Updating Privacy Policy

--- src/routes/privacyPolicy/PrivacyPolicy.jsx
+++ src/routes/privacyPolicy/PrivacyPolicy.jsx
@@ -7,23 +7,105 @@ const PrivacyPolicy = () => {
         <AnimatedPage>
             <div className="xl:px-16 px-1 m-auto max-w-5xl pb-20">
                 <h1 className="text-5xl font-extrabold tracking-tight text-slate-900 text-center mt-10">Privacy Policy</h1>
-                <p className="mt-4 text-base leading-7 text-slate-600 text-center">Last updated on November 3, 2022</p>
-                <div className="relative px-4 sm:px-6 lg:px-8 text-sm text-gray-500 mt-20" style={{ lineHeight: '1.7142857' }}>
+                <p className="mt-4 text-base leading-7 text-slate-600 text-center">Last updated on November 14th, 2022</p>
+                <div className="relative px-4 sm:px-6 lg:px-8 text-sm text-gray-500 mt-12" style={{ lineHeight: '1.7142857' }}>
                     <div className="mx-auto max-w-[40rem] prose-sm prose prose-slate prose-a:font-semibold
                                  prose-a:text-sky-500 hover:prose-a:text-sky-600">
-                        <p>This privacy policy ("Policy") describes how Livetakeoff. ("Livetakeoff", "we", "us" or "our")
-                            collects, protects and uses the personally identifiable information ("Personal Information")
-                            you ("User", "you" or "your") may provide through the Livetakeoff website (livetakeoff.com).
-                            The Policy also describes the choices available to you regarding our use of your Personal
-                            Information and how you can access and update this information. This Policy does not apply
-                            to the practices of companies that we do not own or control, or to individuals that we do not employ
-                            or manage.
+                        <p>www.livetakeoff.com (the "Site") is owned and operated by Live Takeoff LLC. Live Takeoff LLC can be contacted at:
                         </p>
+
+                        <h2 className="text-lg font-medium text-black my-4">Purpose</h2>
+
+                        <p>
+                            The purpose of this privacy policy (this "Privacy Policy") is to inform users of our Site of the following:
+                        </p>
+                        
+                        <ul className="pl-5 py-4 list-disc list-inside space-y-4">
+                            <li>The personal data we will collect</li>
+                            <li>Use of collected data</li>
+                            <li>The rights of Site user</li>
+                            <li>The Site's cookie policy</li>
+                            <li>The Site's cookie policy</li>
+                        </ul>
+
+                        <p className="mt-2">This Privacy Policy applies in addition to the terms and conditions of our Site.</p>
+
+                        <h2 className="text-lg font-medium text-black my-4">Consent</h2>
+                        <p>
+                            By using our Site users agree that they consent to:
+                        </p>
+                        <ul className="pl-5 py-4 list-disc list-inside space-y-4">
+                            <li>The conditions set out in this Privacy Policy</li>
+                            <li>The collection, use, and retention of the data listed in this Privacy Policy</li>
+                        </ul>
+
+                        <h2 className="text-lg font-medium text-black my-4">Personal Data We Collect</h2>
+                        <p>
+                            We only collect data that helps us achieve the purpose set out in this Privacy Policy. We will not collect any additional data beyond the data listed below without notifying you first.
+                        </p>
+
+                        <h2 className="text-lg font-medium text-black my-4">Data Collected in a Non-Automatic Way</h2>
+                        <p>
+                            We may also collect the following data when you perform certain functions on our Site:
+                        </p>
+                        <ul className="pl-5 py-4 list-disc list-inside space-y-4">
+                            <li>Email Address</li>
+                            <li>Phone Number</li>
+                            <li>Address</li>
+                        </ul>
+
+                        <p>This data may be collected using the following methods:</p>
+                        <ol className="pl-5 py-4">
+                            <li>1. Creating the user account</li>
+                        </ol>
+
+                        <h2 className="text-lg font-medium text-black my-4">How We Use Personal Data</h2>
+                        <p>
+                            Data collected on our Site will only be used for the purposes specified in this Privacy Policy or indicated on the relevant pages of our Site. We will not use your data beyond what we disclose in this Privacy Policy.
+                        </p>
+                        <p>
+                            The data we collect when the user performs certain functions may be used for the following purposes: 
+                        </p>
+                        <ol className="pl-5 py-4">
+                            <li>User account creation to be able to use the Web Application</li>
+                        </ol>
+
+
+
+                        <h2 className="text-lg font-medium text-black my-4">Who We Share Personal Data With</h2>
+
+
+                        <h2 className="text-lg font-medium text-black my-4">How Long We Store Personal Data</h2>
+
+
+                        <h2 className="text-lg font-medium text-black my-4">How We Protect Your Personal Data</h2>
+
+
+                        <h2 className="text-lg font-medium text-black my-4">Children</h2>
+
+
+
+                        <h2 className="text-lg font-medium text-black my-4">How to Access, Modify, Delete, or Challenge the Data Collected</h2>
+
+
+                        <h2 className="text-lg font-medium text-black my-4">Do Not Track Notice</h2>
+
+
+                        <h2 className="text-lg font-medium text-black my-4">How to Opt-Out of Data Collection, Use or Disclosure</h2>
+
+
+                        <h2 className="text-lg font-medium text-black my-4">Cookie Policy</h2>
+
+
+                        <h2 className="text-lg font-medium text-black my-4">Modifications</h2>
+
 
                         <h2 className="font-bold mt-6 text-black">Contacting us</h2>
                         <p>If you have any questions about this Policy, please contact us by
                              email at <a href="mailto:support@livetakeoff.com" className="text-sky-500 font-medium">support@livetakeoff.com</a>.
                         </p>
+                        <p>(786) 975-6255</p>
+                        <p>750 SW 34th St, Suite 209, Fort Lauderdale, FL 33315</p>
                     </div>
                 </div>
             </div>
