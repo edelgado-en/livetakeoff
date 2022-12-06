e78f3113ed095c4a54d9a243e5c740a07b3b0c63
Enrique Delgado <edelga34@ford.com>
2022-12-06 11:17:20 -0500

    Changed tile to Open Jobs in Customer home

--- src/routes/home/customer/CustomerHome.jsx
+++ src/routes/home/customer/CustomerHome.jsx
@@ -349,7 +349,7 @@ const CustomerHome = () => {
               <div className="border-b border-t border-gray-200 pl-4 pr-6 pt-4 pb-4 
                               sm:pl-6 lg:pl-8 xl:border-t-0 xl:pl-6 xl:pt-5">
                 <div className="flex items-center">
-                  <h1 className="flex-1 text-lg font-medium">Jobs</h1>
+                  <h1 className="flex-1 text-lg font-medium">Open Jobs</h1>
                   <div className="">
                     <button
                       type="button"
