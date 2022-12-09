07d045a17c32948db1a7210feeec6ab0b3121c50
Enrique Delgado <edelga34@ford.com>
2022-12-09 08:05:34 -0500

    Added range date filters to user productivity dashboard

--- src/routes/home/dashboard/UserProductivity.jsx
+++ src/routes/home/dashboard/UserProductivity.jsx
@@ -10,22 +10,39 @@ import * as api from './apiService'
 import AnimatedPage from '../../../components/animatedPage/AnimatedPage'
 import Loader from '../../../components/loader/Loader'
 
+const dateOptions = [
+    { id: 'yesterday', name: 'Yesterday' },
+    { id: 'last7Days', name: 'Last 7 Days' },
+    { id: 'lastWeek', name: 'Last Week' },
+    { id: 'MTD', name: 'Month to Date' },
+    { id: 'lastMonth', name: 'Last Month' },
+    { id: 'lastQuarter', name: 'Last Quarter' },
+    { id: 'YTD', name: 'Year to Date'},
+    { id: 'lastYear', name: 'Last Year' },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
 const UserProductivity = () => {
     const { id } = useParams();
 
     const [loading, setLoading] = useState(true)
     const [productivityData, setProductivityData] = useState({})
+    const [dateSelected, setDateSelected] = useState(dateOptions[3])
 
     const navigate = useNavigate()
 
     useEffect(() => {
         getUserProductivityStats()
-    }, [])
+    }, [dateSelected])
 
     const getUserProductivityStats = async () => {
-        
+        setLoading(true)
+
         try {
-            const { data } = await api.getUserProductivityStats(id)
+            const { data } = await api.getUserProductivityStats(id, { dateSelected: dateSelected.id })
             
             setProductivityData(data)
             setLoading(false)
@@ -55,26 +72,86 @@ const UserProductivity = () => {
 
                 {!loading && (
                     <div className="py-8">
-                        <div className="flex items-center space-x-5">
-                            <div className="flex-shrink-0">
-                                <div className="relative">
-                                    <img className="h-16 w-16 rounded-full" src={productivityData.user.avatar} alt="" />
-                                    <span className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></span>
+                        <div className="grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1">
+                            <div className="flex items-center space-x-5">
+                                <div className="flex-shrink-0">
+                                    <div className="relative">
+                                        <img className="h-16 w-16 rounded-full" src={productivityData.user.avatar} alt="" />
+                                        <span className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="text-xl font-bold text-gray-700">
+                                        {productivityData.user.first_name} {' '} {productivityData.user.last_name}
+                                    </div>
+                                    <div className="text-sm text-gray-500">Member since
+                                        <span className="text-gray-900 mx-1">
+                                        <ReactTimeAgo date={new Date(productivityData.member_since)} locale="en-US" timeStyle="twitter" />
+                                    </span>
+                                    - 
+                                    <span className="text-gray-900 mx-1">
+                                        {productivityData.user.vendor}
+                                    </span>
+                                    </div>
                                 </div>
                             </div>
-                            <div>
-                                <div className="text-xl font-bold text-gray-700">
-                                    {productivityData.user.first_name} {' '} {productivityData.user.last_name}
-                                </div>
-                                <div className="text-sm text-gray-500">Member since
-                                    <span className="text-gray-900 mx-1">
-                                    <ReactTimeAgo date={new Date(productivityData.member_since)} locale="en-US" timeStyle="twitter" />
-                                  </span>
-                                  - 
-                                  <span className="text-gray-900 mx-1">
-                                    {productivityData.user.vendor}
-                                  </span>
-                                  </div>
+                            <div className="flex xl:justify-end lg:justify-end md:justify-end sm:justify-start xs:justify-start pt-6">
+                                <Listbox value={dateSelected} onChange={setDateSelected}>
+                                    {({ open }) => (
+                                        <>
+                                        <div className="relative" style={{width:'340px'}}>
+                                            <Listbox.Button className="relative w-full cursor-default rounded-md 
+                                                                        bg-white py-2 px-3 pr-8 text-left
+                                                                        shadow-sm border-gray-200 border focus:border-gray-200 focus:ring-0 focus:outline-none
+                                                                        text-lg font-medium leading-6 text-gray-900">
+                                                <span className="block truncate">
+                                                    {dateSelected.name}
+                                                </span>
+                                                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
+                                                    <ChevronDownIcon className="h-4 w-4 text-gray-500" aria-hidden="true" />
+                                                </span>
+                                            </Listbox.Button>
+
+                                            <Transition
+                                                show={open}
+                                                as={Fragment}
+                                                leave="transition ease-in duration-100"
+                                                leaveFrom="opacity-100"
+                                                leaveTo="opacity-0">
+                                                <Listbox.Options className="absolute left-0 z-10 mt-1 max-h-96 w-full overflow-auto
+                                                                            rounded-md bg-white py-1 shadow-lg ring-1
+                                                                            ring-black ring-opacity-5 focus:outline-none text-sm">
+                                                    {dateOptions.map((sort) => (
+                                                        <Listbox.Option
+                                                            key={sort.id}
+                                                            className={({ active }) =>
+                                                                    classNames(active ? 'text-white bg-red-600' : 'text-gray-900',
+                                                                            'relative select-none py-2 pl-3 pr-9 cursor-pointer text-md')}
+                                                            value={sort}>
+                                                            {({ selected, active }) => (
+                                                                <>
+                                                                    <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate text-md')}>
+                                                                        {sort.name}
+                                                                    </span>
+                                                                    {selected ? (
+                                                                        <span
+                                                                            className={classNames(
+                                                                            active ? 'text-white' : 'text-red-600',
+                                                                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                                            )}>
+                                                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                        </span>
+                                                                    ) : null}
+                                                                </>
+                                                            )}
+                                                        </Listbox.Option>
+                                                    ))}
+                                                </Listbox.Options>
+                                            </Transition>
+                                        </div>
+                                        </>
+                                    )}
+                                </Listbox>
                             </div>
                         </div>
 
@@ -147,6 +224,10 @@ const UserProductivity = () => {
                                     <p className="mt-1 max-w-2xl text-sm text-gray-500">Airports where services have been completed.</p>
                                 </div>
                                 <div className="pr-2 text-gray-500">
+                                    {productivityData.top_five_airports.length === 0 && (
+                                        <div className="m-auto text-sm text-center mt-20">No airports found.</div>
+                                    )}
+
                                     {productivityData.top_five_airports.map((airport, index) => (
                                         <div key={index} className="m-auto max-w-sm">
                                             <div className="flex justify-between py-3 text-sm gap-3">
@@ -226,6 +307,10 @@ const UserProductivity = () => {
                                     <p className="mt-1 max-w-2xl text-sm text-gray-500">Data gathered from the last 50 completed services.</p>
                                 </div>
                                 <div className="pr-2 text-gray-500">
+                                    {productivityData.recent_service_stats.length === 0 && (
+                                        <div className="m-auto text-sm text-center mt-20">No services found.</div>
+                                    )}
+                                    
                                     {productivityData.recent_service_stats.map((service, index) => (
                                         <div key={index} className="border-b border-gray-200">
                                             <div className="truncate overflow-ellipsis w-80 font-medium pt-4 text-sm" >{service.service}</div>
@@ -251,6 +336,10 @@ const UserProductivity = () => {
                                     <p className="mt-1 max-w-2xl text-sm text-gray-500">Aircrafts where services have been completed.</p>
                                 </div>
                                 <div className="pr-2 text-gray-500">
+                                    {productivityData.top_five_aircraft_types.length === 0 && (
+                                        <div className="m-auto text-sm text-center mt-20">No aircrafts found.</div>
+                                    )}
+                                    
                                     {productivityData.top_five_aircraft_types.map((aircraft, index) => (
                                         <div key={index} className="m-auto max-w-sm">
                                             <div className="flex justify-between py-3 pb-1 text-sm gap-3">
--- src/routes/home/dashboard/apiService.ts
+++ src/routes/home/dashboard/apiService.ts
@@ -21,6 +21,6 @@ export const getTeamProductivityStats = (data: any) => {
     return httpService.post('/api/team-productivity', data)
 }
 
-export const getUserProductivityStats = (id: number) => {
-    return httpService.get(`/api/user-productivity/${id}/`)
+export const getUserProductivityStats = (id: number, data: any) => {
+    return httpService.post(`/api/user-productivity/${id}/`, data)
 }
