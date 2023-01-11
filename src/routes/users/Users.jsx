6554b18084c74493f07c39118557cd467769292e
Enrique Delgado <edelga34@ford.com>
2023-01-11 09:19:44 -0500

    Added role filter to Team view

--- src/routes/users/Users.jsx
+++ src/routes/users/Users.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useState, useEffect } from 'react'
-import { Dialog, Transition, Switch, Menu } from '@headlessui/react'
-import { ChevronLeftIcon } from '@heroicons/react/outline'
+import { Dialog, Transition, Switch, Menu, Listbox } from '@headlessui/react'
+import { ChevronLeftIcon, ChevronDownIcon, CheckIcon } from '@heroicons/react/outline'
 import Loader from '../../components/loader/Loader'
 import * as api from './apiService'
 
@@ -23,6 +23,18 @@ const MagnifyingGlassIcon = () => {
     )
 }
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const roleOptions = [
+  { id: 'All', name: 'All' },
+  { id: 'Customers', name: 'Customers' },
+  { id: 'Project Managers', name: 'P. Managers' },
+  { id: 'Account Managers', name: 'A. Managers' },
+  { id: 'Admins', name: 'Admins' },
+]
+
 const Users = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
   const [loading, setLoading] = useState(true)
@@ -30,6 +42,7 @@ const Users = () => {
   const [totalUsers, setTotalUsers] = useState(0)
   const [searchText, setSearchText] = useState('')
   const [firstLoad, setFirstLoad] = useState(true)
+  const [roleSelected, setRoleSelected] = useState(roleOptions[0])
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -44,13 +57,14 @@ const Users = () => {
       clearTimeout(timeoutID);
     };
 
-  }, [searchText])
+  }, [searchText, roleSelected])
 
   const searchUsers = async () => {
     setLoading(true)
 
     const request = {
       name: searchText,
+      role: roleSelected.id
     }
 
     try {
@@ -173,8 +187,8 @@ const Users = () => {
                       </div> */}
                     </div>
                     {/* MOBILE */}
-                    <form className="mt-2 flex space-x-4" action="#">
-                      <div className="min-w-0 flex-1">
+                    <form className="mt-2 flex justify-between space-x-4" action="#">
+                      <div className="">
                         <label htmlFor="search" className="sr-only">
                           Search
                         </label>
@@ -193,11 +207,69 @@ const Users = () => {
                             value={searchText}
                             onChange={event => setSearchText(event.target.value)}
                             onKeyDown={handleKeyDown}
-                            className="block w-full rounded-md border-gray-300 pl-10 focus:border-sky-500
+                            className="block w-full rounded-md border-gray-500 pl-10 focus:border-sky-500
                                     focus:ring-sky-500 text-xs"
                             placeholder="Search name..."
                           />
                         </div>
+                      </div>
+                      <div>
+                        <Listbox value={roleSelected} onChange={setRoleSelected}>
+                          {({ open }) => (
+                              <>
+                              <div className="relative" style={{width: '120px'}}>
+                                  <Listbox.Button className="relative w-full cursor-default rounded-md 
+                                                                bg-white py-2 px-3 pr-8 text-left
+                                                               border-gray-500 shadow-sm text-gray-600
+                                                              text-xs" style={{borderWidth: '1px'}}>
+                                      <span className="block truncate">
+                                          {roleSelected.name}
+                                      </span>
+                                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
+                                          <ChevronDownIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
+                                      </span>
+                                  </Listbox.Button>
+
+                                  <Transition
+                                      show={open}
+                                      as={Fragment}
+                                      leave="transition ease-in duration-100"
+                                      leaveFrom="opacity-100"
+                                      leaveTo="opacity-0">
+                                      <Listbox.Options className="absolute left-0 z-10 mt-1 max-h-72 w-full overflow-auto
+                                                                  rounded-md bg-white py-1 shadow-lg ring-1
+                                                                  ring-black ring-opacity-5 focus:outline-none text-xs">
+                                          {roleOptions.map((role) => (
+                                              <Listbox.Option
+                                                  key={role.id}
+                                                  className={({ active }) =>
+                                                          classNames(active ? 'text-white bg-red-600' : 'text-gray-900',
+                                                                  'relative cursor-default select-none py-2 pl-3 pr-9')}
+                                                  value={role}>
+                                                  {({ selected, active }) => (
+                                                      <>
+                                                          <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                                              {role.name}
+                                                          </span>
+                                                          {selected ? (
+                                                              <span
+                                                                  className={classNames(
+                                                                  active ? 'text-white' : 'text-red-600',
+                                                                  'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                                  )}>
+                                                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                              </span>
+                                                          ) : null}
+                                                      </>
+                                                  )}
+                                              </Listbox.Option>
+                                          ))}
+                                      </Listbox.Options>
+                                  </Transition>
+                              </div>
+                              </>
+                          )}
+                        </Listbox>
                       </div>
                     </form>
                     {/* Directory list Mobile */}
@@ -272,7 +344,7 @@ const Users = () => {
               </article>
             </main>
             <aside className="hidden w-96 flex-shrink-0 border-r border-gray-200 xl:order-first xl:flex xl:flex-col">
-              <div className="px-6 pt-6 pb-4">
+              <div className="px-6 pr-4 pt-6 pb-4">
                 <div className="flex justify-between">
                   <div className="flex gap-2">
                     <h2 className="text-2xl font-medium text-gray-900">Team</h2>
@@ -294,7 +366,7 @@ const Users = () => {
                   </div>
                 </div>
                 <form className="mt-3 flex space-x-4" action="#">
-                  <div className="min-w-0 flex-1">
+                  <div className="">
                     <label htmlFor="search" className="sr-only">
                       Search
                     </label>
@@ -318,6 +390,64 @@ const Users = () => {
                         placeholder="Search name..."
                       />
                     </div>
+                  </div>
+                  <div>
+                    <Listbox value={roleSelected} onChange={setRoleSelected}>
+                      {({ open }) => (
+                          <>
+                          <div className="relative" style={{width: '120px'}}>
+                              <Listbox.Button className="relative w-full cursor-default rounded-md 
+                                                            bg-white py-2.5 px-3 pr-8 text-left
+                                                            border-gray-300 shadow-sm text-gray-600
+                                                          text-xs" style={{borderWidth: '1px'}}>
+                                  <span className="block truncate">
+                                      {roleSelected.name}
+                                  </span>
+                                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
+                                      <ChevronDownIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
+                                  </span>
+                              </Listbox.Button>
+
+                              <Transition
+                                  show={open}
+                                  as={Fragment}
+                                  leave="transition ease-in duration-100"
+                                  leaveFrom="opacity-100"
+                                  leaveTo="opacity-0">
+                                  <Listbox.Options className="absolute left-0 z-10 mt-1 max-h-72 w-full overflow-auto
+                                                              rounded-md bg-white py-1 shadow-lg ring-1
+                                                              ring-black ring-opacity-5 focus:outline-none text-xs">
+                                      {roleOptions.map((role) => (
+                                          <Listbox.Option
+                                              key={role.id}
+                                              className={({ active }) =>
+                                                      classNames(active ? 'text-white bg-red-600' : 'text-gray-900',
+                                                              'relative cursor-default select-none py-2 pl-3 pr-9')}
+                                              value={role}>
+                                              {({ selected, active }) => (
+                                                  <>
+                                                      <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                                          {role.name}
+                                                      </span>
+                                                      {selected ? (
+                                                          <span
+                                                              className={classNames(
+                                                              active ? 'text-white' : 'text-red-600',
+                                                              'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                              )}>
+                                                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                          </span>
+                                                      ) : null}
+                                                  </>
+                                              )}
+                                          </Listbox.Option>
+                                      ))}
+                                  </Listbox.Options>
+                              </Transition>
+                          </div>
+                          </>
+                      )}
+                    </Listbox>
                   </div>
                 </form>
               </div>
