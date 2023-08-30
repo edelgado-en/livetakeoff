18b954c65a0863071b74e2c789f2269fbca5c11b
edelgado-git <enriquedelgado806@gmail.com>
2023-08-30 15:16:30 -0400

    Adjusted label to Adjust & confirm

--- src/routes/inventory/AdjustItemModal.jsx
+++ src/routes/inventory/AdjustItemModal.jsx
@@ -91,7 +91,7 @@ const AdjustItemModal = ({
                               bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700
                                 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
           >
-            Adjust
+            Adjust & Confirm
           </button>
 
           <button
