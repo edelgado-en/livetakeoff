ca9db567dd13e33224c0ea0f05d1a2006566b505
edelgado-git <enriquedelgado806@gmail.com>
2023-11-13 09:18:08 -0500

    Add actual dates to job activity instead of using relative time

--- src/routes/job/JobActivityFeed.jsx
+++ src/routes/job/JobActivityFeed.jsx
@@ -1,154 +1,225 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
-import AnimatedPage from '../../components/animatedPage/AnimatedPage';
-import * as api from './apiService'
-import ReactTimeAgo from 'react-time-ago'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import AnimatedPage from "../../components/animatedPage/AnimatedPage";
+import * as api from "./apiService";
+import ReactTimeAgo from "react-time-ago";
 import { CheckIcon, UserIcon } from "@heroicons/react/outline";
 import { PencilIcon } from "@heroicons/react/solid";
 
-
 export default function Example() {
-    const { jobId } = useParams()
-    const [activities, setActivities] = useState([])
+  const { jobId } = useParams();
+  const [activities, setActivities] = useState([]);
 
-    useEffect(() => {
-        getActivities()
-    }, [])
-    
-    const getActivities = async () => {
-        const { data } = await api.getJobActivities(jobId)
-        
-        setActivities(data.results)
-    }
-  
-  
-    return (
-        <AnimatedPage>
-        <div className="mt-6 max-w-3xl m-auto px-2">
-            <div className="flex flex-row">
-                <div className="flex-1">
-                    <h1 className="text-2xl font-semibold text-gray-600">Job Activity</h1>
-                </div>
-            </div>
-            <div className="flow-root mt-6">
-                <ul className="-mb-8">
-                    {activities.map((activity, eventIdx) => (
-                        <li key={activity.id}>
-                            <div className="relative pb-8">
-                            {eventIdx !== activities.length - 1 ? (
-                                <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
-                            ) : null}
-                            
-                            <div className="relative flex space-x-3">
-                                <div>
-                                {activity.activity_type === 'S' && (
-                                    <span
-                                        className={`
-                                        ${activity.status === 'A' && 'bg-sky-500 '}
-                                        ${activity.status === 'S' && 'bg-yellow-500 '}
-                                        ${activity.status === 'U' && 'bg-indigo-500 '}
-                                        ${activity.status === 'W' && 'bg-green-500 '}
-                                        ${activity.status === 'R' && 'bg-purple-500 '}
-                                        ${activity.status === 'C' && 'bg-blue-500 '}
-                                        ${activity.status === 'T' && 'bg-black '}
-                                        ${activity.status === 'I' && 'bg-gray-400 '}
-                                        ${activity.status === 'P' && 'bg-red-500 '}
-                                        ${activity.status === 'X' && 'bg-gray-500'}
-                                        h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white`
-                                        }
-                                    >
-                                        <span className="text-white">{activity.status}</span>
-                                    </span>
-                                )}
+  useEffect(() => {
+    getActivities();
+  }, []);
 
-                                {activity.activity_type !== 'S' && (
-                                    <span className={`bg-gray-400 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white`}>
-                                        <span className="text-white"><PencilIcon className="h-4 w-4" /></span>
-                                    </span>
-                                )}
-                                
-                                </div>
-                                <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                                    {activity.activity_type === 'S' && (
-                                        <div>
-                                            <p className="text-sm text-gray-500">
-                                                {activity.status !== 'P' && (
-                                                    <>
-                                                    <span>Status changed to </span> 
-                                                    <span className="font-medium text-black">
-                                                        {activity.status === 'A' && 'Accepted'}
-                                                        {activity.status === 'S' && 'Assigned'}
-                                                        {activity.status === 'U' && 'Submitted'}
-                                                        {activity.status === 'W' && 'Work In Progress'}
-                                                        {activity.status === 'C' && 'Completed'}
-                                                        {activity.status === 'T' && 'Canceled'}
-                                                        {activity.status === 'R' && 'Review'}
-                                                        {activity.status === 'I' && 'Invoiced'}
-                                                        {activity.status === 'X' && 'Unassign'}
-                                                    </span>
-                                                    </>
-                                                )}
+  const getActivities = async () => {
+    const { data } = await api.getJobActivities(jobId);
 
-                                                {activity.status === 'P' && (
-                                                    <>
-                                                        <span className="text-sm text-gray-500">
-                                                            Price changed to
-                                                        </span>
-                                                        <span className="font-medium text-black ml-1">${activity.price}</span>
-                                                    </>
-                                                )}
-                                                <span className="ml-1">by {activity.user.first_name} {activity.user.last_name}</span>
-                                            </p>
-                                        </div>
-                                    )}
+    //iterate through data.results and format the timestamp date
+    data.results.forEach((activity) => {
+      activity.timestamp = new Date(activity.timestamp);
+      //use military time instead of am/pm
+      activity.timestamp = activity.timestamp.toLocaleTimeString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      });
+    });
 
-                                    {(activity.activity_type === 'P') && (
-                                        <div>
-                                            <p  className="text-sm text-left text-gray-500">
-                                                <span className="text-sm text-gray-500">
-                                                    Price changed to
-                                                </span>
-                                                <span className="font-medium text-black ml-1">${activity.price}</span>
-                                                <span className="ml-1">by {activity.user.first_name} {activity.user.last_name}</span>
-                                            </p>
-                                        </div>
-                                    )}
+    setActivities(data.results);
+  };
 
-                                    {(activity.activity_type !== 'P' && activity.activity_type !== 'S') && (
-                                        <div>
-                                            <p  className="text-sm text-left text-gray-500">
-                                                <span className="font-medium text-black">
-                                                    {activity.activity_type === 'E' && 'Departure'}
-                                                    {activity.activity_type === 'A' && 'Arrival'}
-                                                    {activity.activity_type === 'B' && 'Complete Before'}
-                                                    {activity.activity_type === 'O' && 'Airport'}
-                                                    {activity.activity_type === 'F' && 'FBO'}
-                                                    {activity.activity_type === 'T' && 'Tail Number'}
-                                                    {activity.activity_type === 'U' && 'Photos Uploaded'}
-                                                    {activity.activity_type === 'R' && 'Job Returned'}
-                                                </span>
-                                                
-                                                {activity.activity_type !== 'U' && (
-                                                    <span> changed </span> 
-                                                )}
-                                                
-                                                <span className="ml-1">by {activity.user.first_name} {activity.user.last_name}</span>
-                                            </p>
-                                        </div>
-                                    )}
-
-                                    <div className="whitespace-nowrap text-right text-sm text-gray-500">
-                                        <ReactTimeAgo date={new Date(activity.timestamp)} locale="en-US" timeStyle="twitter" />
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  return (
+    <AnimatedPage>
+      <div className="mt-6 max-w-3xl m-auto px-2">
+        <div className="flex flex-row">
+          <div className="flex-1">
+            <h1 className="text-2xl font-semibold text-gray-600">
+              Job Activity
+            </h1>
+          </div>
         </div>
-        </AnimatedPage>
-    
-  )
+        <div className="flow-root mt-6">
+          <ul className="-mb-8">
+            {activities.map((activity, eventIdx) => (
+              <li key={activity.id}>
+                <div className="relative pb-8">
+                  {eventIdx !== activities.length - 1 ? (
+                    <span
+                      className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
+                      aria-hidden="true"
+                    />
+                  ) : null}
+
+                  <div className="relative flex space-x-3">
+                    <div>
+                      {activity.activity_type === "S" && (
+                        <span
+                          className={`
+                                        ${
+                                          activity.status === "A" &&
+                                          "bg-sky-500 "
+                                        }
+                                        ${
+                                          activity.status === "S" &&
+                                          "bg-yellow-500 "
+                                        }
+                                        ${
+                                          activity.status === "U" &&
+                                          "bg-indigo-500 "
+                                        }
+                                        ${
+                                          activity.status === "W" &&
+                                          "bg-green-500 "
+                                        }
+                                        ${
+                                          activity.status === "R" &&
+                                          "bg-purple-500 "
+                                        }
+                                        ${
+                                          activity.status === "C" &&
+                                          "bg-blue-500 "
+                                        }
+                                        ${
+                                          activity.status === "T" && "bg-black "
+                                        }
+                                        ${
+                                          activity.status === "I" &&
+                                          "bg-gray-400 "
+                                        }
+                                        ${
+                                          activity.status === "P" &&
+                                          "bg-red-500 "
+                                        }
+                                        ${
+                                          activity.status === "X" &&
+                                          "bg-gray-500"
+                                        }
+                                        h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white`}
+                        >
+                          <span className="text-white">{activity.status}</span>
+                        </span>
+                      )}
+
+                      {activity.activity_type !== "S" && (
+                        <span
+                          className={`bg-gray-400 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white`}
+                        >
+                          <span className="text-white">
+                            <PencilIcon className="h-4 w-4" />
+                          </span>
+                        </span>
+                      )}
+                    </div>
+                    <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                      {activity.activity_type === "S" && (
+                        <div>
+                          <p className="text-sm text-gray-500">
+                            {activity.status !== "P" && (
+                              <>
+                                <span>Status changed to </span>
+                                <span className="font-medium text-black">
+                                  {activity.status === "A" && "Accepted"}
+                                  {activity.status === "S" && "Assigned"}
+                                  {activity.status === "U" && "Submitted"}
+                                  {activity.status === "W" &&
+                                    "Work In Progress"}
+                                  {activity.status === "C" && "Completed"}
+                                  {activity.status === "T" && "Canceled"}
+                                  {activity.status === "R" && "Review"}
+                                  {activity.status === "I" && "Invoiced"}
+                                  {activity.status === "X" && "Unassign"}
+                                </span>
+                              </>
+                            )}
+
+                            {activity.status === "P" && (
+                              <>
+                                <span className="text-sm text-gray-500">
+                                  Price changed to
+                                </span>
+                                <span className="font-medium text-black ml-1">
+                                  ${activity.price}
+                                </span>
+                              </>
+                            )}
+                            <span className="ml-1">
+                              by {activity.user.first_name}{" "}
+                              {activity.user.last_name}
+                            </span>
+                          </p>
+                        </div>
+                      )}
+
+                      {activity.activity_type === "P" && (
+                        <div>
+                          <p className="text-sm text-left text-gray-500">
+                            <span className="text-sm text-gray-500">
+                              Price changed to
+                            </span>
+                            <span className="font-medium text-black ml-1">
+                              ${activity.price}
+                            </span>
+                            <span className="ml-1">
+                              by {activity.user.first_name}{" "}
+                              {activity.user.last_name}
+                            </span>
+                          </p>
+                        </div>
+                      )}
+
+                      {activity.activity_type !== "P" &&
+                        activity.activity_type !== "S" && (
+                          <div>
+                            <p className="text-sm text-left text-gray-500">
+                              <span className="font-medium text-black">
+                                {activity.activity_type === "E" && "Departure"}
+                                {activity.activity_type === "A" && "Arrival"}
+                                {activity.activity_type === "B" &&
+                                  "Complete Before"}
+                                {activity.activity_type === "O" && "Airport"}
+                                {activity.activity_type === "F" && "FBO"}
+                                {activity.activity_type === "T" &&
+                                  "Tail Number"}
+                                {activity.activity_type === "U" &&
+                                  "Photos Uploaded"}
+                                {activity.activity_type === "R" &&
+                                  "Job Returned"}
+                              </span>
+
+                              {activity.activity_type !== "U" && (
+                                <span> changed </span>
+                              )}
+
+                              <span className="ml-1">
+                                by {activity.user.first_name}{" "}
+                                {activity.user.last_name}
+                              </span>
+                            </p>
+                          </div>
+                        )}
+
+                      <div className="whitespace-nowrap text-right text-sm text-gray-500">
+                        {/* <ReactTimeAgo
+                          date={new Date(activity.timestamp)}
+                          locale="en-US"
+                          timeStyle="twitter"
+                        /> */}
+                        {activity.timestamp}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </AnimatedPage>
+  );
 }
