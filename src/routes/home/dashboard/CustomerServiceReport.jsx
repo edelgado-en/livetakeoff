57b1cd5771661a10624326b9ec26ce928a4ee470
edelgado-git <enriquedelgado806@gmail.com>
2024-03-20 10:04:34 -0400

    Removed customer dropdown from service report for external pms

--- src/routes/home/dashboard/CustomerServiceReport.jsx
+++ src/routes/home/dashboard/CustomerServiceReport.jsx
@@ -1791,16 +1791,17 @@ export default function CustomerServiceReport() {
                 </Listbox>
               </div>
               <div>
-                {customers.length > 1 && (
-                  <Listbox
-                    value={customerSelected}
-                    onChange={setCustomerSelected}
-                  >
-                    {({ open }) => (
-                      <>
-                        <div className="relative">
-                          <Listbox.Button
-                            className={`relative w-full cursor-default rounded-md border 
+                {customers.length > 1 &&
+                  !currentUser.isExternalProjectManager && (
+                    <Listbox
+                      value={customerSelected}
+                      onChange={setCustomerSelected}
+                    >
+                      {({ open }) => (
+                        <>
+                          <div className="relative">
+                            <Listbox.Button
+                              className={`relative w-full cursor-default rounded-md border 
                             ${
                               customerSelected &&
                               customerSelected.name !== "All Customers"
@@ -1809,141 +1810,141 @@ export default function CustomerServiceReport() {
                             }                          
                                       py-2 pl-3 pr-10 text-left
                                         shadow-sm  sm:text-md`}
-                          >
-                            <span className="block truncate">
-                              {customerSelected
-                                ? customerSelected.name
-                                : "Select customer"}
-                            </span>
-                            <span
-                              className={`pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ${
-                                customerSelected &&
-                                customerSelected.name !== "All Customers"
-                                  ? "text-white"
-                                  : "text-gray-400"
-                              }`}
                             >
-                              <ChevronUpDownIcon
-                                className="h-5 w-5"
-                                aria-hidden="true"
-                              />
-                            </span>
-                          </Listbox.Button>
+                              <span className="block truncate">
+                                {customerSelected
+                                  ? customerSelected.name
+                                  : "Select customer"}
+                              </span>
+                              <span
+                                className={`pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ${
+                                  customerSelected &&
+                                  customerSelected.name !== "All Customers"
+                                    ? "text-white"
+                                    : "text-gray-400"
+                                }`}
+                              >
+                                <ChevronUpDownIcon
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            </Listbox.Button>
 
-                          <Transition
-                            show={open}
-                            as={Fragment}
-                            leave="transition ease-in duration-100"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                          >
-                            <Listbox.Options
-                              className="absolute z-10 mt-1 max-h-96 w-full overflow-auto
+                            <Transition
+                              show={open}
+                              as={Fragment}
+                              leave="transition ease-in duration-100"
+                              leaveFrom="opacity-100"
+                              leaveTo="opacity-0"
+                            >
+                              <Listbox.Options
+                                className="absolute z-10 mt-1 max-h-96 w-full overflow-auto
                                                                         rounded-md bg-white py-1 text-base shadow-lg ring-1
                                                                         ring-black ring-opacity-5 focus:outline-none sm:text-md"
-                            >
-                              <div className="relative">
-                                <div className="sticky top-0 z-20  px-1">
-                                  <div className="mt-1 block  items-center">
-                                    <input
-                                      type="text"
-                                      name="search"
-                                      id="search"
-                                      value={customerSearchTerm}
-                                      onChange={(e) =>
-                                        setCustomerSearchTerm(e.target.value)
-                                      }
-                                      className="shadow-sm border px-2 bg-gray-50 focus:ring-sky-500
+                              >
+                                <div className="relative">
+                                  <div className="sticky top-0 z-20  px-1">
+                                    <div className="mt-1 block  items-center">
+                                      <input
+                                        type="text"
+                                        name="search"
+                                        id="search"
+                                        value={customerSearchTerm}
+                                        onChange={(e) =>
+                                          setCustomerSearchTerm(e.target.value)
+                                        }
+                                        className="shadow-sm border px-2 bg-gray-50 focus:ring-sky-500
                                                                         focus:border-sky-500 block w-full py-2 pr-12 sm:text-md
                                                                         border-gray-300 rounded-md"
-                                    />
-                                    <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5 ">
-                                      {customerSearchTerm && (
+                                      />
+                                      <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5 ">
+                                        {customerSearchTerm && (
+                                          <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-6 w-6 text-blue-500 font-bold mr-1"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                            onClick={() => {
+                                              handleClearCustomerSearchFilter();
+                                            }}
+                                          >
+                                            <path
+                                              fillRule="evenodd"
+                                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                              clipRule="evenodd"
+                                            />
+                                          </svg>
+                                        )}
                                         <svg
                                           xmlns="http://www.w3.org/2000/svg"
-                                          className="h-6 w-6 text-blue-500 font-bold mr-1"
-                                          viewBox="0 0 20 20"
-                                          fill="currentColor"
-                                          onClick={() => {
-                                            handleClearCustomerSearchFilter();
-                                          }}
+                                          className="h-6 w-6 text-gray-500 mr-1"
+                                          fill="none"
+                                          viewBox="0 0 24 24"
+                                          stroke="currentColor"
                                         >
                                           <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                                           />
                                         </svg>
-                                      )}
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-6 w-6 text-gray-500 mr-1"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                      >
-                                        <path
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                          strokeWidth="2"
-                                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                        />
-                                      </svg>
+                                      </div>
                                     </div>
                                   </div>
                                 </div>
-                              </div>
-                              {filteredCustomers.map((customer) => (
-                                <Listbox.Option
-                                  key={customer.id}
-                                  className={({ active }) =>
-                                    classNames(
-                                      active
-                                        ? "text-white bg-red-600"
-                                        : "text-gray-900",
-                                      "relative cursor-default select-none py-2 pl-3 pr-9"
-                                    )
-                                  }
-                                  value={customer}
-                                >
-                                  {({ selected, active }) => (
-                                    <>
-                                      <span
-                                        className={classNames(
-                                          selected
-                                            ? "font-semibold"
-                                            : "font-normal",
-                                          "block truncate"
-                                        )}
-                                      >
-                                        {customer.name}
-                                      </span>
-                                      {selected ? (
+                                {filteredCustomers.map((customer) => (
+                                  <Listbox.Option
+                                    key={customer.id}
+                                    className={({ active }) =>
+                                      classNames(
+                                        active
+                                          ? "text-white bg-red-600"
+                                          : "text-gray-900",
+                                        "relative cursor-default select-none py-2 pl-3 pr-9"
+                                      )
+                                    }
+                                    value={customer}
+                                  >
+                                    {({ selected, active }) => (
+                                      <>
                                         <span
                                           className={classNames(
-                                            active
-                                              ? "text-white"
-                                              : "text-red-600",
-                                            "absolute inset-y-0 right-0 flex items-center pr-4"
+                                            selected
+                                              ? "font-semibold"
+                                              : "font-normal",
+                                            "block truncate"
                                           )}
                                         >
-                                          <CheckIcon
-                                            className="h-5 w-5"
-                                            aria-hidden="true"
-                                          />
+                                          {customer.name}
                                         </span>
-                                      ) : null}
-                                    </>
-                                  )}
-                                </Listbox.Option>
-                              ))}
-                            </Listbox.Options>
-                          </Transition>
-                        </div>
-                      </>
-                    )}
-                  </Listbox>
-                )}
+                                        {selected ? (
+                                          <span
+                                            className={classNames(
+                                              active
+                                                ? "text-white"
+                                                : "text-red-600",
+                                              "absolute inset-y-0 right-0 flex items-center pr-4"
+                                            )}
+                                          >
+                                            <CheckIcon
+                                              className="h-5 w-5"
+                                              aria-hidden="true"
+                                            />
+                                          </span>
+                                        ) : null}
+                                      </>
+                                    )}
+                                  </Listbox.Option>
+                                ))}
+                              </Listbox.Options>
+                            </Transition>
+                          </div>
+                        </>
+                      )}
+                    </Listbox>
+                  )}
               </div>
             </div>
 
