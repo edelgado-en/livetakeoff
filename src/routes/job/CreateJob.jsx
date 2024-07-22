30d485ca8f2879729ddb71bc46db9045cc6f088b
edelgado-git <enriquedelgado806@gmail.com>
2024-07-22 19:15:08 -0400

    Added fbo hours of operation alert when creating a job

--- src/routes/job/CreateJob.jsx
+++ src/routes/job/CreateJob.jsx
@@ -1899,6 +1899,28 @@ const CreateJob = () => {
                         </div>
                       </div>
                     )}
+                    {fboSelected && fboSelected.hours_of_operation && (
+                      <div className="rounded-md bg-red-50 p-4 mt-2">
+                        <div className="flex">
+                          <div className="flex-shrink-0">
+                            <ExclamationCircleIcon
+                              className="h-5 w-5 text-red-400"
+                              aria-hidden="true"
+                            />
+                          </div>
+                          <div className="ml-3">
+                            <h3 className="text-sm font-medium text-red-800">
+                              Hours of operation
+                            </h3>
+                            <div className="mt-2 text-sm text-red-700">
+                              <ul className="list-disc space-y-1 pl-5">
+                                {fboSelected.hours_of_operation}
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
                 <div className="space-y-6">
