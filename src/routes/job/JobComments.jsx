6030588f8c73e5de31b5611c85aa907123582a2e
edelgado-git <enriquedelgado806@gmail.com>
2024-08-19 08:32:57 -0400

    Added update comments button

--- src/routes/job/JobComments.jsx
+++ src/routes/job/JobComments.jsx
@@ -17,6 +17,25 @@ import { toast } from "react-toastify";
 
 import DeleteCommentModal from "./DeleteJobCommentModal";
 
+const ReloadIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-5 h-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+      />
+    </svg>
+  );
+};
+
 const JobComments = () => {
   const { jobId } = useParams();
   const bottomRef = useRef(null);
@@ -238,10 +257,26 @@ const JobComments = () => {
             location.pathname.includes("comments") ? "mt-8" : ""
           } relative overflow-hidden rounded-lg border border-gray-300`}
         >
-          <div className="p-4 bg-gray-100">
-            <h3 className="text-base font-bold leading-7 text-gray-900 uppercase">
-              Comments
-            </h3>
+          <div className="grid grid-cols-2 gap-4 p-4 bg-gray-100">
+            <div>
+              <h3 className="text-base font-bold leading-7 text-gray-900 uppercase">
+                Comments
+              </h3>
+            </div>
+            <div className="text-right">
+              <button
+                type="button"
+                onClick={() => getComments()}
+                disabled={loading}
+                className="items-center justify-center 
+                                    rounded-md bg-white px-4 py-2.5
+                                    text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                    sm:w-auto inline-flex"
+              >
+                <ReloadIcon />
+                <span className="ml-2">Get Latest comments</span>
+              </button>
+            </div>
           </div>
           <div className="border-t border-gray-200">
             <div
