2091a2070c895846b96d0dd97878d6381b2425c2
edelgado-git <enriquedelgado806@gmail.com>
2024-08-19 08:42:23 -0400

    Added update queue button to completed list

--- src/routes/job/CompleteList.jsx
+++ src/routes/job/CompleteList.jsx
@@ -52,6 +52,25 @@ const MagnifyingGlassIcon = () => {
   );
 };
 
+const ReloadIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+      />
+    </svg>
+  );
+};
+
 const XMarkIcon = () => {
   return (
     <svg
@@ -702,6 +721,20 @@ const CompleteList = () => {
                                      text-xs focus:border-transparent focus:ring-0"
               placeholder="search by tail or PO..."
             />
+          </div>
+          <div className="hidden xl:block lg:block md:block text-center">
+            <button
+              type="button"
+              onClick={() => searchJobs()}
+              disabled={loading}
+              className="items-center justify-center 
+                                    rounded-md bg-white px-4 py-2
+                                    text-xs font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                    sm:w-auto inline-flex"
+            >
+              <ReloadIcon />
+              <span className="ml-2">Update Queue</span>
+            </button>
           </div>
           <div className="flex gap-2">
             <div className="">
@@ -1804,6 +1837,21 @@ const CompleteList = () => {
               </button>
             </div>
           </div>
+        </div>
+
+        <div className="xl:hidden lg:hidden md:hidden xs:block sm:block my-4 text-center">
+          <button
+            type="button"
+            onClick={() => searchJobs()}
+            disabled={loading}
+            className="items-center justify-center 
+                                    rounded-md bg-white px-4 py-2.5
+                                    text-xs font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                    sm:w-auto inline-flex"
+          >
+            <ReloadIcon />
+            <span className="ml-2">Update Queue</span>
+          </button>
         </div>
 
         <div className="flex gap-4">
