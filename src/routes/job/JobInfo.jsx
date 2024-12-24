1c227dfc19c23bde796ceb92ce4a297b858d2f34
edelgado-git <enriquedelgado806@gmail.com>
2024-12-23 20:22:59 -0500

    Job Attachment delete button only visible to admins

--- src/routes/job/JobInfo.jsx
+++ src/routes/job/JobInfo.jsx
@@ -1459,16 +1459,18 @@ const JobInfo = () => {
                             </div>
                           </div>
                           <div className="flex gap-2">
-                            <button
-                              type="button"
-                              onClick={() => handleDeleteJobFile(file)}
-                              className="inline-flex w-full justify-center rounded-md border
-                                                        border-gray-300 bg-white px-2 py-1 text-base 
-                                                        text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2
-                                                        focus:ring-gray-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
-                            >
-                              Delete
-                            </button>
+                            {isAdmin && (
+                              <button
+                                type="button"
+                                onClick={() => handleDeleteJobFile(file)}
+                                className="inline-flex w-full justify-center rounded-md border
+                                                            border-gray-300 bg-white px-2 py-1 text-base 
+                                                            text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2
+                                                            focus:ring-gray-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+                              >
+                                Delete
+                              </button>
+                            )}
                             <button
                               type="button"
                               onClick={() => downloadFile(file)}
