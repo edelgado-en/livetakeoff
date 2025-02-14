7ffce34ba6d94229f21c5582386731168ccd8a14
edelgado-git <enriquedelgado806@gmail.com>
2025-02-14 08:45:13 -0500

    grid line effect removed

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -463,7 +463,7 @@ const Login = () => {
             </dl>
             <div className="mt-8 relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
               <div className="absolute inset-0 -z-10 overflow-hidden">
-                <svg
+                {/* <svg
                   aria-hidden="true"
                   className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
                 >
@@ -491,7 +491,7 @@ const Login = () => {
                     height="100%"
                     strokeWidth={0}
                   />
-                </svg>
+                </svg> */}
               </div>
               <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
                 <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
