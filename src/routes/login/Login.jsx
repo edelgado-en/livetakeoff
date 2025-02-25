5cbda9ddf0e3820161bea87080be0519f9a2d6e6
edelgado-git <enriquedelgado806@gmail.com>
2025-02-25 14:24:03 -0500

    New section added

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -69,7 +69,6 @@ const features = [
       "Instantly identify VIP aircraft, high-priority requests, and vendor-accepted jobs.",
     icon: TagIcon,
   },
-
   {
     name: "Manage jobs and schedules from anywhere",
     description:
@@ -807,6 +806,47 @@ const Login = () => {
                         </div>
                       </div>
                     ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="">
+              <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                  <h1 className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                    The Ultimate One-Stop Solution for Aircraft Detailing
+                  </h1>
+                  <p className="mx-auto mt-5 max-w-prose text-2xl text-gray-700 leading-relaxed ">
+                    With LiveTakeoff, you’re not just managing jobs—you’re
+                    running a smarter, more efficient operation that integrates
+                    real-time flight tracking, job automation, and vendor
+                    coordination into one seamless experience.
+                  </p>
+                  <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <Link
+                      to="/shared/contact"
+                      className="rounded-md bg-red-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                    >
+                      Get started
+                    </Link>
+                    <Link
+                      to="/shared/contact"
+                      className="text-lg font-semibold text-gray-900"
+                    >
+                      Request Demo <span aria-hidden="true">→</span>
+                    </Link>
+                  </div>
+                </div>
+                <div className="mt-16 flow-root sm:mt-24">
+                  <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                    <img
+                      alt="App screenshot"
+                      src="https://res.cloudinary.com/datidxeqm/image/upload/v1740505532/website/image002_yaituf.png"
+                      width={2432}
+                      height={1442}
+                      className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                    />
                   </div>
                 </div>
               </div>
