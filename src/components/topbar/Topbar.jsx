892207ebd530e8ca4502dc3acf0d98aaad4f8f9a
edelgado-git <enriquedelgado806@gmail.com>
2025-05-19 08:12:09 -0400

    Removed hovering effect from top navigation bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -291,7 +291,6 @@ const Topbar = () => {
                           {({ open }) => (
                             <>
                               <Popover.Button
-                                onMouseEnter={() => toggleDashboardMenu()}
                                 onClick={() => toggleDashboardMenu()}
                                 className={classNames(
                                   open ? "text-white" : "text-white",
@@ -318,10 +317,7 @@ const Topbar = () => {
                                 leaveFrom="opacity-100 translate-y-0"
                                 leaveTo="opacity-0 translate-y-1"
                               >
-                                <Popover.Panel
-                                  onMouseLeave={() => setDashboardMenu(false)}
-                                  className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2"
-                                >
+                                <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
                                   <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                                     <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                                       {dashboards.map((item) => (
@@ -358,7 +354,6 @@ const Topbar = () => {
                           {({ open }) => (
                             <>
                               <Popover.Button
-                                onMouseEnter={() => toggleMoreMenu()}
                                 onClick={() => toggleMoreMenu()}
                                 className={classNames(
                                   open ? "text-white" : "text-white",
@@ -385,10 +380,7 @@ const Topbar = () => {
                                 leaveFrom="opacity-100 translate-y-0"
                                 leaveTo="opacity-0 translate-y-1"
                               >
-                                <Popover.Panel
-                                  onMouseLeave={() => setShowMoreMenu(false)}
-                                  className="absolute z-10 -ml-4 mt-3 w-screen max-w-6xl transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2"
-                                >
+                                <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-6xl transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
                                   <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                                     <div className="relative grid grid-cols-3 gap-y-6 gap-x-24 bg-white px-5 py-12 sm:gap-8 sm:p-8">
                                       {moreOptions.map((item) => (
@@ -500,7 +492,6 @@ const Topbar = () => {
                           {({ open }) => (
                             <>
                               <Popover.Button
-                                onMouseEnter={() => toggleDashboardMenu()}
                                 onClick={() => toggleDashboardMenu()}
                                 className={classNames(
                                   open ? "text-white" : "text-white",
@@ -527,10 +518,7 @@ const Topbar = () => {
                                 leaveFrom="opacity-100 translate-y-0"
                                 leaveTo="opacity-0 translate-y-1"
                               >
-                                <Popover.Panel
-                                  onMouseLeave={() => setDashboardMenu(false)}
-                                  className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2"
-                                >
+                                <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
                                   <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                                     <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                                       {internalCoordinatorDashboards.map(
