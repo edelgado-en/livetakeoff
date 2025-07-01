e32c2e37e0c5b6a6a22fe5fa32acc2edcbc0fdaf
edelgado-git <enriquedelgado806@gmail.com>
2025-06-30 21:12:34 -0400

    adjusted css

--- src/routes/home/dashboard/CustomerTails.jsx
+++ src/routes/home/dashboard/CustomerTails.jsx
@@ -200,7 +200,7 @@ const CustomerTails = () => {
                       leaveTo="opacity-0"
                     >
                       <Listbox.Options
-                        className="absolute z-10 mt-1 max-h-96 w-full overflow-auto
+                        className="absolute z-20 mt-1 max-h-96 w-full overflow-auto
                                                                             rounded-md bg-white py-1 text-base shadow-lg ring-1
                                                                             ring-black ring-opacity-5 focus:outline-none sm:text-md"
                       >
@@ -295,7 +295,7 @@ const CustomerTails = () => {
                       leaveTo="opacity-0"
                     >
                       <Listbox.Options
-                        className="absolute z-10 mt-1 max-h-96 w-full overflow-auto
+                        className="absolute z-20 mt-1 max-h-96 w-full overflow-auto
                                                                             rounded-md bg-white py-1 text-base shadow-lg ring-1
                                                                             ring-black ring-opacity-5 focus:outline-none sm:text-md"
                       >
@@ -442,7 +442,7 @@ const CustomerTails = () => {
 
         {!loading && tails.length > 0 && (
           <div className="mt-8 flow-root">
-            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
               <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                 <table className="min-w-full divide-y divide-gray-300">
                   <thead>
