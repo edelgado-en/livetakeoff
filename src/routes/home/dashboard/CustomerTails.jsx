f05b880d8a5adf8b075a366dad8dcb168e00253f
edelgado-git <enriquedelgado806@gmail.com>
2025-07-14 12:59:42 -0400

    Added notes to customer tail

--- src/routes/home/dashboard/CustomerTails.jsx
+++ src/routes/home/dashboard/CustomerTails.jsx
@@ -8,6 +8,7 @@ import {
   ChevronDownIcon,
   ClockIcon,
   NewspaperIcon,
+  ChatAltIcon,
 } from "@heroicons/react/outline";
 
 import { Listbox, Transition, Menu } from "@headlessui/react";
@@ -127,6 +128,12 @@ const CustomerTails = () => {
     name: "All Customers",
   });
   const [customerSearchTerm, setCustomerSearchTerm] = useState("");
+
+  const [isNotesModalOpen, setIsNotesModalOpen] = useState(false);
+
+  const [selectedTail, setSelectedTail] = useState(null);
+
+  const [currentNote, setCurrentNote] = useState("");
 
   useEffect(() => {
     //Basic throttling
@@ -286,6 +293,33 @@ const CustomerTails = () => {
     }
   };
 
+  const updateCustomerTailNotes = async () => {
+    const request = {
+      notes: currentNote,
+    };
+    try {
+      const { data } = await api.updateCustomerTail(selectedTail.id, request);
+      if (data) {
+        const updatedTails = tails.map((t) =>
+          t.id === selectedTail.id
+            ? {
+                ...t,
+                notes: data.notes,
+              }
+            : t
+        );
+        setTails(updatedTails);
+        toast.success("Note Added!");
+      }
+
+      setIsNotesModalOpen(false);
+      setCurrentNote("");
+      setSelectedTail(null);
+    } catch (err) {
+      toast.error("Unable to add note");
+    }
+  };
+
   const getCustomerTailStats = async () => {
     setLoading(true);
     const request = {
@@ -344,7 +378,6 @@ const CustomerTails = () => {
                 className="inline-flex items-center justify-center 
                                 rounded-md border border-transparent bg-red-600 px-4 py-2
                                 text-sm font-medium text-white shadow-sm hover:bg-red-700
-                                focus:outline-none focus:ring-2 focus:ring-red-500
                                 focus:ring-offset-2 sm:w-auto"
               >
                 Update All Customers
@@ -988,7 +1021,30 @@ const CustomerTails = () => {
                                   {tail.status === "S" && "Service Due"}
                                 </p>
                               </div>
-                              <div>
+                              <div className="flex gap-3">
+                                <div>
+                                  <ChatAltIcon
+                                    className={`h-5 w-5
+                                                ${
+                                                  tail.notes?.length > 0
+                                                    ? "text-red-600"
+                                                    : "text-gray-500"
+                                                }
+                                                hover:
+                                                ${
+                                                  tail.notes?.length > 0
+                                                    ? "hover:text-red-700"
+                                                    : "hover:text-gray-900"
+                                                }
+                                         cursor-pointer`}
+                                    onClick={() => {
+                                      setSelectedTail(tail);
+                                      setCurrentNote(tail.notes || "");
+                                      setIsNotesModalOpen(true);
+                                    }}
+                                    aria-hidden="true"
+                                  />
+                                </div>
                                 <Menu as="div" className="relative flex-none">
                                   <Menu.Button className="relative block text-gray-500 hover:text-gray-900">
                                     <span className="absolute -inset-2.5" />
@@ -1044,6 +1100,61 @@ const CustomerTails = () => {
                       ))}
                     </tbody>
                   </table>
+                  {isNotesModalOpen && (
+                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+                        <h2 className="font-medium mb-2 text-lg">Notes</h2>
+                        <div className="flex justify-between items-center mb-4">
+                          <div>
+                            <div className="font-medium text-gray-900">
+                              {selectedTail.tail_number}{" "}
+                              <span className="text-gray-500 text-xs">
+                                {selectedTail.ident}
+                              </span>
+                            </div>
+                            <div className=" text-gray-500 text-xs">
+                              {selectedTail.aircraft_type_name}
+                            </div>
+                          </div>
+                          <div>
+                            <div className="text-gray-700 text-xs">
+                              Updated:
+                              <ReactTimeAgo
+                                date={new Date(selectedTail.last_updated)}
+                                locale="en-US"
+                                timeStyle="twitter"
+                              />{" "}
+                            </div>
+                          </div>
+                        </div>
+
+                        <textarea
+                          className="w-full border border-gray-300 rounded p-2 mb-4"
+                          rows={6}
+                          value={currentNote}
+                          onChange={(e) => setCurrentNote(e.target.value)}
+                        />
+                        <div className="flex justify-end space-x-2">
+                          <button
+                            className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300"
+                            onClick={() => setIsNotesModalOpen(false)}
+                          >
+                            Cancel
+                          </button>
+                          <button
+                            className="inline-flex items-center justify-center 
+                                rounded-md border border-transparent bg-red-600 px-4 py-2
+                                text-sm font-medium text-white shadow-sm hover:bg-red-700
+                                focus:outline-none focus:ring-2 focus:ring-red-500
+                                focus:ring-offset-2 sm:w-auto"
+                            onClick={() => updateCustomerTailNotes()}
+                          >
+                            Save
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
